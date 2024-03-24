a393e7c208448f32f6beffd0ac014f5e63b45f43
Yosif43 <yosifyosifov4393@GMAIL.COM>
2024-03-24 09:54:15 +0000

    Create calculations.py

---
+++ Python-Fundamentals/04.Functions - Lab/calculations.py
@@ -0,0 +1,17 @@
+input_operator = input()
+first_num = int(input())
+second_num = int(input())
+
+def solve(a, b, operator):
+    result = None
+    if operator == "multiply":
+        result = a * b
+    elif operator == "divide":
+        result = int(a / b)
+    elif operator == "add":
+        result = a + b
+    elif operator == "subtract":
+        result = a - b
+    return result
+
+print(solve(first_num, second_num, input_operator))
