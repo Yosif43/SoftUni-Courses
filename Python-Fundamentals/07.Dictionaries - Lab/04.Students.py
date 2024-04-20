a712806994c609ce7146f615b976cfa66d18c787
Yosif43 <yosifyosifov4393@GMAIL.COM>
2024-04-20 13:14:24 +0100

    Create 04.Students.py

---
+++ Python-Fundamentals/07.Dictionaries - Lab/04.Students.py
@@ -0,0 +1,15 @@
+students = []
+course_to_search = ""
+while True:
+    student_info = input()
+
+    if ":" not in student_info:
+        course_to_search = student_info
+        break
+
+    name, ID, course = student_info.split(":")
+    students.append({"name": name, "ID": ID, "course": course})
+
+for student in students:
+    if course_to_search.startswith(student["course"]):
+        print(f"{student['name']} - {student['ID']}")
