d741de588742b6c658c37672d523ef1bb66d819f
Yosif43 <yosifyosifov4393@GMAIL.COM>
2024-04-30 17:26:54 +0100

    Create 01. Valid Usernames.py

---
+++ Python-Fundamentals/08.Text Processing - Exercise/01. Valid Usernames.py
@@ -0,0 +1,26 @@
+def lenght_is_valid(username):
+    if 3 <= len(username) <= 16:
+        return True
+    return False
+
+def characters_are_valid(username):
+    for character in username:
+        if not (character.isalnum() or character == "-" or character == "_"):
+            return False
+    return True
+
+def no_redundant_symbols(username):
+    if " " in username:
+        return False
+    return True
+
+def username_is_valid(username):
+    if lenght_is_valid(username) and characters_are_valid(username) and no_redundant_symbols(username):
+        return True
+    return False
+
+
+usernames = input().split(", ")
+for username in usernames:
+    if username_is_valid(username):
+        print(username)
