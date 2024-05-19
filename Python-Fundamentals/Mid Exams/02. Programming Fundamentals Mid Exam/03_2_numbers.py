8e2bf162397be906a8a7994a31efbbf3aad75740
Yosif43 <yosifyosifov4393@GMAIL.COM>
2024-05-19 09:30:03 +0100

    Create 03_2_numbers.py

---
+++ Python-Fundamentals/Mid Exams/02. Programming Fundamentals Mid Exam/03_2_numbers.py
@@ -0,0 +1,11 @@
+numbers = [int(j) for j in input().split()]
+
+average = sum(numbers) / len(numbers)
+
+final = [j for j in numbers if j > average]
+
+if len(final) >= 1:
+    sorted_final = sorted(final, reverse=True)
+    print(*sorted_final[0:5])
+else:
+    print("No")
