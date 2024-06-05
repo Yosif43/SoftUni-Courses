b57b2ad0d52a812d4260e0d7d3a116200c0653db
Yosif43 <yosifyosifov4393@GMAIL.COM>
2024-06-05 17:40:52 +0100

    Create 07_projects_creation.py

---
+++ Python Basic/1. First Steps In Coding/Lab/07_projects_creation.py
@@ -0,0 +1,4 @@
+name = input()
+project_count = int(input())
+total_project_time = project_count * 3
+print(f"The architect {name} will need {total_project_time} hours to complete {project_count} project/s.")
