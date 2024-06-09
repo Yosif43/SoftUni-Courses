0f39ffe2f24ad74e17ff002f48bc30b7549396ec
Yosif43 <yosifyosifov4393@GMAIL.COM>
2024-06-09 09:10:16 +0100

    Create 08_basketball_equipment.py

---
+++ Python Basic/1. First Steps In Coding/Exercise/08_basketball_equipment.py
@@ -0,0 +1,10 @@
+
+yearly_tax = int(input())
+sneakers = (yearly_tax * 0.6)
+outfit = (sneakers * 0.8)
+basketball_balls = (outfit * 0.25)
+accessories = (basketball_balls * 0.2)
+
+total_price = (yearly_tax + sneakers + outfit + basketball_balls + accessories)
+
+print(total_price)
