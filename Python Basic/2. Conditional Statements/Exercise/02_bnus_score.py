e1b47ed5834f02adbe7ab5efaa2dff503a14491f
Yosif43 <yosifyosifov4393@GMAIL.COM>
2024-06-17 16:16:48 +0100

    Create 02_bnus_score.py

---
+++ Python Basic/2. Conditional Statements/Exercise/02_bnus_score.py
@@ -0,0 +1,23 @@
+number = int(input())
+bonus = 0
+
+if number <= 100:
+    bonus += 5
+elif number <= 1000:
+    bonus = number * 0.2
+elif number > 1000:
+    bonus = number * 0.1
+
+if number % 2 == 0:
+    bonus = bonus + 1
+    print(bonus)
+    print(bonus + number)
+elif number % 10 == 5:
+    bonus = bonus + 2
+    print(bonus)
+    print(bonus + number)
+else:
+    print(bonus)
+    print(bonus + number)
+
+
