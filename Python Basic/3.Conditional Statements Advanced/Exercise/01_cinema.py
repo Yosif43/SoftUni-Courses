70367038401f5114941bdcf95b762447de27e4b9
Yosif43 <yosifyosifov4393@GMAIL.COM>
2024-07-01 17:14:24 +0100

    Create 01_cinema.py

---
+++ Python Basic/3.Conditional Statements Advanced/Exercise/01_cinema.py
@@ -0,0 +1,14 @@
+screen_type = input()
+rows = int(input())
+columns = int(input())
+
+income = 0
+cinema_capacity = rows * columns
+if screen_type == 'Premiere':
+    income = cinema_capacity * 12.00
+elif screen_type == 'Normal':
+    income = cinema_capacity * 7.50
+elif screen_type == 'Discount':
+    income = cinema_capacity * 5.00
+
+print(f'{income:.2f} leva')
