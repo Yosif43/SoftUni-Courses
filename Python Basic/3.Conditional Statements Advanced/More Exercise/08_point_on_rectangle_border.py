69976f4456b5dd64b4ca645c2d2f9fd819ae120e
Yosif43 <yosifyosifov4393@GMAIL.COM>
2024-07-06 17:30:38 +0100

    Create 08_point_on_rectangle_border.py

---
+++ Python Basic/3.Conditional Statements Advanced/More Exercise/08_point_on_rectangle_border.py
@@ -0,0 +1,13 @@
+x1 = float(input())
+y1 = float(input())
+x2 = float(input())
+y2 = float(input())
+x = float(input())
+y = float(input())
+
+on_border = (x == x1 or x == x2) and y1 <= y <= y2 or (y == y1 or y == y2) and x1 <= x <= x2
+
+if on_border:
+    print("Border")
+else:
+    print("Inside / Outside")
