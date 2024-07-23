c88d367cb2a30d2f6e056ece5c349e768fa836f6
Yosif43 <yosifyosifov4393@GMAIL.COM>
2024-07-23 17:26:13 +0100

    Create 08_graduation.py

---
+++ Python Basic/5.While Loop/Lab/08_graduation.py
@@ -0,0 +1,18 @@
+name = input()
+average = 0
+fails = 0
+student_class = 1
+
+while True:
+    grade = float(input())
+    if grade < 4:
+        fails += 1
+        if fails >= 2:
+            print(f"{name} has been excluded at {student_class} grade")
+            break
+        continue
+    student_class += 1
+    average += grade
+    if student_class > 12:
+        print(f"{name} graduated. Average grade: {average / 12:.2f}")
+        break
