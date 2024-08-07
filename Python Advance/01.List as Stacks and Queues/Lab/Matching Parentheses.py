5fe6b6a4bfd57cbb07fa2d464657a343e5547d07
Yosif43 <yosifyosifov4393@GMAIL.COM>
2024-08-07 10:16:11 +0100

    Create Matching Parentheses.py

---
+++ Python Advance/01.List as Stacks and Queues/Lab/Matching Parentheses.py
@@ -0,0 +1,10 @@
+expression = input()
+stack = []
+
+for index in range(len(expression)):
+    if expression[index] == "(":
+        stack.append(index)
+    elif expression[index] == ")":
+        start_index = stack.pop()
+        last_index = index + 1
+        print(expression[start_index:last_index])
