3a5707562ad1f1b3ee343aa35d163d7fc02f2d01
Yosif43 <yosifyosifov4393@GMAIL.COM>
2024-08-17 08:12:14 +0100

    Create 5.Santa's Present Factory.py

---
+++ Python Advance/02.2.StackQueuesTuplesSets/Exercise/5.Santa's Present Factory.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+materials = [int(x) for x in input().split()]
+magic = deque(int(x) for x in input().split())
+
+points = {150: 'Doll', 250: 'Wooden train', 300: 'Teddy bear', 400: 'Bicycle'}
+presents = {}
+
+while materials and magic:
+    total_magic = materials[-1] * magic[0]
+    if total_magic in points:
+        new_present = points[total_magic]
+        if new_present not in presents:
+            presents[new_present] = 0
+        presents[new_present] += 1
+        materials.pop()
+        magic.popleft()
+    elif total_magic < 0:
+        materials.append(materials.pop() + magic.popleft())
+    elif total_magic > 0:
+        magic.popleft()
+        materials[-1] += 15
+    elif materials[-1] == 0 and magic[0] == 0:
+        materials.pop()
+        magic.popleft()
+    elif materials[-1] == 0:
+        materials.pop()
+    elif magic[0] == 0:
+        magic.popleft()
+
+if ('Doll' in presents and 'Wooden train' in presents) or ('Teddy bear' in presents and 'Bicycle' in presents):
+    print("The presents are crafted! Merry Christmas!")
+else:
+    print("No presents this Christmas!")
+if materials:
+    print(f"Materials left: {', '.join([str(x) for x in materials[::-1]])}")
+if magic:
+    print(f"Magic left: {', '.join([str(x) for x in magic])}")
+
+for key, value in sorted(presents.items()):
+    print(f"{key}: {value}")
