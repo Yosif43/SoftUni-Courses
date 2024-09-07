00aeb137621330be0f5ba05cea4e74b055b78ae3
Yosif43 <yosifyosifov4393@GMAIL.COM>
2024-09-07 12:27:58 +0100

    Create 09. Recursion Palindrome.py

---
+++ Python Advance/04.Functions Advanced/Exercise/09. Recursion Palindrome.py
@@ -0,0 +1,11 @@
+def palindrome(word, index):
+    if index == len(word) // 2:
+        return f"{word} is a palindrome"
+    if word[index] != word[-1 - index]:
+        return f"{word} is not a palindrome"
+    return palindrome(word, index + 1)
+
+
+print(palindrome("abcba", 0))
+print()
+print(palindrome("peter", 0))
