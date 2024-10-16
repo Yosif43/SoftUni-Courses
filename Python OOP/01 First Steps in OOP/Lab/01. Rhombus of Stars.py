c0437079435b5caddef5063c5bccca7fcf8a4772
Yosif43 <yosifyosifov4393@GMAIL.COM>
2024-10-16 17:22:25 +0100

    Create 01. Rhombus of Stars.py

---
+++ Python OOP/01 First Steps in OOP/Lab/01. Rhombus of Stars.py
@@ -0,0 +1,18 @@
+def print_row(n, row):
+    print(" " * (n - row), end="")
+    print(*['*'] * row)
+
+def print_triangle(n):
+    for row in range(1, n + 1):
+        print_row(n, row)
+
+def print_reverse_triangle(n):
+    for row in range(n - 1, 0, -1):
+        print_row(n, row)
+
+def create_rhombus(n):
+    print_triangle(n)
+    print_reverse_triangle(n)
+
+
+create_rhombus(int(input()))
