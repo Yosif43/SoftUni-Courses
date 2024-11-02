95e664455fb3f188a6def21cbb27f505d9f302bb
Yosif43 <yosifyosifov4393@GMAIL.COM>
2024-11-02 17:46:24 +0000

    Create band.py

---
+++ Python OOP/02 Classes and Objects/Exercise/09_spoopify/project/band.py
@@ -0,0 +1,29 @@
+from project.album import Album
+from project.song import Song
+
+
+class Band:
+    def __init__(self, name: str):
+        self.name = name
+        self.albums = []
+
+    def add_album(self, album: Album):
+        if album in self.albums:
+            return f"Band {self.name} already has {album.name} in their library."
+        self.albums.append(album)
+        return f"Band {self.name} has added their newest album {album.name}."
+
+    def remove_album(self, album_name: str):
+        for album in self.albums:
+            if album.name == album_name:
+                if album.published:
+                    return "Album has been published. It cannot be removed."
+                self.albums.remove(album)
+                return f"Album {album_name} has been removed."
+        return f"Album {album_name} is not found."
+
+    def details(self):
+        details = [f"Band {self.name}"]
+        for album in self.albums:
+            details.append(album.details())
+        return "\n".join(details)
