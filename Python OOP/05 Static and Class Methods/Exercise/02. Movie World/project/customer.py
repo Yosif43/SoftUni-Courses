e0d1ab74308394bacdc79b4e82e54d3b5249c38c
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-01-22 17:44:37 +0000

    Create customer.py

---
+++ Python OOP/05 Static and Class Methods/Exercise/02. Movie World/project/customer.py
@@ -0,0 +1,14 @@
+from project.dvd import DVD
+from typing import List
+
+
+class Customer:
+    def __init__(self, name: str, age: int, id: int):
+        self.name = name
+        self.age = age
+        self.id = id
+        self.rented_dvds: List[DVD] = []
+
+    def __repr__(self):
+        return (f"{self.id}: {self.name} of age {self.age} has {len(self.rented_dvds)} "
+                f"rented DVD's ({', '.join([d.name for d in self.rented_dvds])})")
