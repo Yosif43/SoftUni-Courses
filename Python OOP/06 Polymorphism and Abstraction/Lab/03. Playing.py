e70b1c5cdbf74bb95f5aee79d9b84d5089c63458
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-01-31 16:40:56 +0000

    Create 03. Playing.py

---
+++ Python OOP/06 Polymorphism and Abstraction/Lab/03. Playing.py
@@ -0,0 +1,17 @@
+def start_playing(instance):
+    return instance.play()
+
+
+class Guitar:
+    def play(self):
+        return "Playing the guitar"
+
+guitar = Guitar()
+print(start_playing(guitar))  # Output: Playing the guitar
+
+class Children:
+    def play(self):
+        return "Children are playing"
+
+children = Children()
+print(start_playing(children))  # Output: Children are playing
