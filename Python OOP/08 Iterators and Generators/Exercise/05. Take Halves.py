de61008954f81b351a8175d1e3d013bff97fcc9b
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-02-20 20:49:58 +0000

    Create 05. Take Halves.py

---
+++ Python OOP/08 Iterators and Generators/Exercise/05. Take Halves.py
@@ -0,0 +1,26 @@
+def solution():
+    def integers():
+        num = 1
+        while True:
+            yield num
+            num += 1
+
+    def halves():
+        for i in integers():
+            yield i / 2
+
+    def take(n, seq):
+        result = []
+        for _ in range(n):
+            result.append(next(seq))
+        return result
+        # return [next(seq) for _ in range(n)]
+    return (take, halves, integers)
+
+take = solution()[0]
+halves = solution()[1]
+print(take(5, halves()))
+print()
+take = solution()[0]
+halves = solution()[1]
+print(take(0, halves()))
