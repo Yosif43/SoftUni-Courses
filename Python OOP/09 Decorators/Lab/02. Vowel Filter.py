f49d52f76c60aa14453214c25eda382628a7d1d4
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-02-22 17:42:28 +0000

    Create 02. Vowel Filter.py

---
+++ Python OOP/09 Decorators/Lab/02. Vowel Filter.py
@@ -0,0 +1,12 @@
+def vowel_filter(function):
+    def wrapper():
+        result = function()
+        vowels = [char for char in result if char.lower() in 'aeiuti']
+        return vowels
+    return wrapper
+
+@vowel_filter
+def get_letters():
+    return ["a", "b", "c", "d", "e"]
+
+print(get_letters())
