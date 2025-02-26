e267ba40eec765637a732dc378bc6b8a90a92c48
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-02-26 09:15:41 +0000

    Create 08. Execution Time.py

---
+++ Python OOP/09 Decorators/Exercise/08. Execution Time.py
@@ -0,0 +1,34 @@
+import time
+
+def exec_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        return execution_time
+
+    return wrapper
+
+@exec_time
+def loop(start, end):
+    total = 0
+    for x in range(start, end):
+        total += x
+    return total
+print(loop(1, 10000000))
+print()
+@exec_time
+def concatenate(strings):
+    result = ""
+    for string in strings:
+        result += string
+    return result
+print(concatenate(["a" for i in range(1000000)]))
+print()
+@exec_time
+def loop():
+    count = 0
+    for i in range(1, 9999999):
+        count += 1
+print(loop())
