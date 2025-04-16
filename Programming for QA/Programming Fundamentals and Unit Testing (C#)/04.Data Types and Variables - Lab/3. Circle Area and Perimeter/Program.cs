ce7164d2c0da8e90486ff2f11721cecc331ad069
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-04-16 19:35:51 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/04.Data Types and Variables - Lab/3. Circle Area and Perimeter/Program.cs
@@ -0,0 +1,5 @@
+﻿double radius = Convert.ToDouble(Console.ReadLine());
+double area = radius * radius * Math.PI;
+double perimeter = 2 * Math.PI * radius;
+Console.WriteLine($"Area = {area:F2}");
+Console.WriteLine($"Perimeter = {perimeter:F2}");
