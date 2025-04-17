2d4b89a935f6cc141bc68e41ebc5b7dfe02da411
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-04-17 16:54:44 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/04.Data Types and Variables - Lab/6. Pets Food/Program.cs
@@ -0,0 +1,6 @@
+﻿int dogfood = int.Parse(Console.ReadLine());
+int catfood = int.Parse(Console.ReadLine());
+double dogpack = dogfood * 2.50;
+double catpack = catfood * 4.00;
+double sum = dogpack + catpack;
+Console.WriteLine($"{sum:F2} lv.");
