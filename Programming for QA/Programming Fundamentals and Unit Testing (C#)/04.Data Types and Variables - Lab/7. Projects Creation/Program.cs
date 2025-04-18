2147cd619aee3b8491827a472c750021d0f5e8a1
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-04-18 09:29:56 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/04.Data Types and Variables - Lab/7. Projects Creation/Program.cs
@@ -0,0 +1,4 @@
+﻿string arch = Console.ReadLine();
+int projects = int.Parse(Console.ReadLine());
+double hours = projects * 3;
+Console.WriteLine($"The architect {arch} will need {hours} hours to complete {projects} project/s.");
