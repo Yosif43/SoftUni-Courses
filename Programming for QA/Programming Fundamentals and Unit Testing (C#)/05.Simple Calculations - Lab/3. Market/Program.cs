dffd4738b1459c5bd0af1c3126d48bbe6f71bf17
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-04-19 08:25:45 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/05.Simple Calculations - Lab/3. Market/Program.cs
@@ -0,0 +1,8 @@
+﻿float tprice = float.Parse(Console.ReadLine());
+float tquantity = float.Parse(Console.ReadLine());
+float cprice = float.Parse(Console.ReadLine());
+float cquantity  = float.Parse(Console.ReadLine());
+double tomatoes = tprice * tquantity;
+double cucumbers = cprice * cquantity;
+double total = tomatoes + cucumbers;
+Console.WriteLine($"{total:F2}");
