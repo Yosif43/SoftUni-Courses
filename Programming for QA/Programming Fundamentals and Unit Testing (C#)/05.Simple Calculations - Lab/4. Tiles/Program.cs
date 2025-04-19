dd129cc539dce5172e760690d12908b2c1c6a11e
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-04-19 08:25:59 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/05.Simple Calculations - Lab/4. Tiles/Program.cs
@@ -0,0 +1,8 @@
+﻿float W = float.Parse(Console.ReadLine());
+float H = float.Parse(Console.ReadLine());
+float Wt = float.Parse(Console.ReadLine());
+float Ht = float.Parse(Console.ReadLine());
+double bathroom = (W * H) * 1.10;
+double area = Wt * Ht;
+double total = Math.Round(bathroom / area);
+Console.WriteLine(total);
