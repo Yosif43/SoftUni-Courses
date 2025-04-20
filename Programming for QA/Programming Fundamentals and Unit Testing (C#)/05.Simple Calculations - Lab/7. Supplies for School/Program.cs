f30477f5974266f8d009fd593bc8bfaefa7964f7
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-04-20 08:09:54 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/05.Simple Calculations - Lab/7. Supplies for School/Program.cs
@@ -0,0 +1,13 @@
+﻿int penPackages = int.Parse(Console.ReadLine());
+int marketPackages = int.Parse(Console.ReadLine());
+int boardCleaner = int.Parse(Console.ReadLine());
+int discount = int.Parse(Console.ReadLine());
+
+double packagePens = penPackages * 5.80;
+double packageMarker = marketPackages * 7.20;
+double cleanerBoards = boardCleaner * 1.20;
+double totalSum = packagePens + packageMarker + cleanerBoards;
+double percent = totalSum * discount / 100;
+double total = totalSum - percent;
+
+Console.WriteLine(total);
