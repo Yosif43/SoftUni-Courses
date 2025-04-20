9a7d54750e726de56a1a7268e333c609372dfb47
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-04-20 08:09:17 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/05.Simple Calculations - Lab/5. Deposit Calculator/Program.cs
@@ -0,0 +1,9 @@
+﻿float deposit = float.Parse(Console.ReadLine());
+int months = int.Parse(Console.ReadLine());
+float annual = float.Parse(Console.ReadLine());
+double persent = annual / 100;
+double amount = deposit * persent;
+double interest = amount / 12;
+double total = deposit + months * interest;
+Console.WriteLine($"{total:F2}");
+
