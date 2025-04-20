1939f8c93bcaf6d530a81f258c80e7d42838b666
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-04-20 08:09:37 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/05.Simple Calculations - Lab/6. Mandatory Literature/Program.cs
@@ -0,0 +1,8 @@
+﻿int pages = int.Parse(Console.ReadLine());
+int pageperHour = int.Parse(Console.ReadLine());
+int days = int.Parse(Console.ReadLine());
+
+int hoursNeed = pages / pageperHour;
+int daysNeedet = hoursNeed / days;
+
+Console.WriteLine(daysNeedet);
