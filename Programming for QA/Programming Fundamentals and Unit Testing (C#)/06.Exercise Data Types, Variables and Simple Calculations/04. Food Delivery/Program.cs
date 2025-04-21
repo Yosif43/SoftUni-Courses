356eef642ede0c98dd5e05d3995ba6676d587777
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-04-21 09:27:20 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/06.Exercise Data Types, Variables and Simple Calculations/04. Food Delivery/Program.cs
@@ -0,0 +1,12 @@
+﻿double chickenMenu = double.Parse(Console.ReadLine());
+double fishMenu = double.Parse(Console.ReadLine());
+double vegetarianMenu = double.Parse(Console.ReadLine());
+
+double chickenPrice = chickenMenu * 10.35;
+double fishPrice = fishMenu * 12.40;
+double vegetarianPrice = vegetarianMenu * 8.15;
+double totalCost = chickenPrice + fishPrice + vegetarianPrice;
+double priceDessert = totalCost * 0.20;
+double totalSum = totalCost + priceDessert + 2.50;
+
+Console.WriteLine($"{totalSum}");
