bb5f3b881787150f0dcd7615326e4273b53f16b0
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-04-21 09:26:06 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/06.Exercise Data Types, Variables and Simple Calculations/01. Reversed Numbers/Program.cs
@@ -0,0 +1,7 @@
+﻿double num1 = double.Parse(Console.ReadLine());
+double num2 = double.Parse(Console.ReadLine());
+double num3 = double.Parse(Console.ReadLine());
+
+Console.WriteLine(num3);
+Console.WriteLine(num2);
+Console.WriteLine(num1);
