08322d6525900bda7a9259e2f9e053f0bb7e349c
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-04-21 09:26:48 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/06.Exercise Data Types, Variables and Simple Calculations/03. Redecorating/Program.cs
@@ -0,0 +1,15 @@
+﻿int nylon = int.Parse(Console.ReadLine());
+int paint = int.Parse(Console.ReadLine());
+int quantityThinner = int.Parse(Console.ReadLine());
+int needetHours = int.Parse(Console.ReadLine());
+
+double paintafterPercent = paint + (paint * 0.10);
+double amountNylon = (nylon + 2) * 1.50;
+double amountPaint = paintafterPercent * 14.50;
+double amountThinner = quantityThinner * 5.00;
+double totalAmount = amountPaint + amountNylon + amountThinner + 0.40;
+double amountCraftsman = (totalAmount * 0.30) * needetHours;
+double totalSum = totalAmount + amountCraftsman;
+
+
+Console.WriteLine($"{totalSum}");
