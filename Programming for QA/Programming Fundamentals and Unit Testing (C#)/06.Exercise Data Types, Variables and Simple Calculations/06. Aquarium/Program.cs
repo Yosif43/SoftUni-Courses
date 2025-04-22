01d2c8ab4c0dd99f6f9ba58306cdf52d6f27861b
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-04-22 16:43:45 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/06.Exercise Data Types, Variables and Simple Calculations/06. Aquarium/Program.cs
@@ -0,0 +1,11 @@
+﻿int lenght = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+double percentage = double.Parse(Console.ReadLine());
+
+double aquariumVolume = lenght * width * height;
+double volumeinLiters = aquariumVolume * 0.001;
+double percentCalc = percentage / 100;
+double requiredLiters = volumeinLiters * (1 - percentCalc);
+
+Console.WriteLine($"{requiredLiters:F2}");
