d790843473658451d6cff9ee0ba1274b28ba8f35
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-04-22 16:43:20 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/06.Exercise Data Types, Variables and Simple Calculations/05. Basketball Equipment/Program.cs
@@ -0,0 +1,8 @@
+﻿int basketball = int.Parse(Console.ReadLine());
+
+double sneakers = basketball - (basketball * 0.40);
+double team = sneakers - (sneakers * 0.20);
+double basketballPrice = team / 4;
+double accessories = basketballPrice / 5;
+double totalPrice = basketball + sneakers + team + basketballPrice + accessories;
+Console.WriteLine(totalPrice);
