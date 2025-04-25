eff4cd08d369d1068e7f0de24e4930e3e502db3e
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-04-25 16:48:57 +0100

    Create CalculateTests.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/07.1.Resources-Unit-Testing-Methods/TestApp.UnitTests/CalculateTests.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class CalculateTests
+{
+    [Test]
+    public void Test_Addition()
+    {
+        // Arrange
+        Calculate calculator = new();
+
+        // Act
+        int actual = calculator.Addition(5, 2);
+
+        // Assert
+        Assert.AreEqual(7, actual, "Addition did not work properly.");
+    }
+
+    [Test]
+    public void Test_Subtraction()
+    {
+        Calculate calculate = new Calculate();
+        int actualResult = calculate.Subtraction(5, 2);
+        Assert.That(3 == actualResult);
+    }
+}
+ 
