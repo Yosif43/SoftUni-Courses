8a690f9eefdbc07231cb0bbe4ede22a174a7fa49
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-04-26 08:42:18 +0100

    Create FactorialTests.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/07.1.Resources-Unit-Testing-Methods/TestApp.UnitTests/FactorialTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class FactorialTests
+{
+    [Test]
+    public void Test_CalculateFactorial_InputZero_ReturnsOne()
+    {
+        int factorial = 0;
+        int actualResult = Factorial.CalculateFactorial(factorial);
+        int expectedResult = 1;
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+
+    [Test]
+    public void Test_CalculateFactorial_InputPositiveNumber_ReturnsCorrectFactorial()
+    {
+        int factorial = 5;
+        int actualResult = Factorial.CalculateFactorial(factorial);
+        int expectedResult = 120;
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
