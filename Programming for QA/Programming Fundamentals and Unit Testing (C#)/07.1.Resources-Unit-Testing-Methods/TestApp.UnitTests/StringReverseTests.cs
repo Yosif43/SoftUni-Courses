03d1bf660b75014f4cedb46a8788d635db3dbdca
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-04-27 08:54:46 +0100

    Create StringReverseTests.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/07.1.Resources-Unit-Testing-Methods/TestApp.UnitTests/StringReverseTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class StringReverseTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_Reverse_WhenGivenEmptyString_ReturnsEmptyString()
+    {
+        // Arrange
+        string emptyString = string.Empty;
+        // Act
+        string actualResult = StringReverse.Reverse(emptyString);
+
+        // Assert
+        Assert.AreEqual(string.Empty, actualResult);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenSingleCharacterString_ReturnsSameCharacter()
+    {
+        string letter = "y";
+        string actualResult = StringReverse.Reverse(letter);
+        Assert.AreEqual(letter, actualResult);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenNormalString_ReturnsReversedString()
+    {
+        string text = "yosko";
+        string actualResult = StringReverse.Reverse(text);
+        string expectedResult = "oksoy";
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
