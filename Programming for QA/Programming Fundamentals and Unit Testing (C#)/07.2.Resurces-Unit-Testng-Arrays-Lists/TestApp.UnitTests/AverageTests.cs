314dd63f288f8716552a89ce6a5ad8b324d889d3
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-01 19:03:03 +0100

    Create AverageTests.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/07.2.Resurces-Unit-Testng-Arrays-Lists/TestApp.UnitTests/AverageTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class AverageTests
+{
+    // TODO: finish the test
+    [Test]
+    public void Test_CalculateAverage_InputHasOneElement_ShouldReturnSameElement()
+    {
+        // Arrange
+        int[] array = { 42 };
+        double expected = 42;
+
+        // Act
+        double result = Average.CalculateAverage(array);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_CalculateAverage_InputHasPositiveIntegers_ShouldReturnCorrectAverage()
+    {
+        // Arrange
+        int[] array = { 10, 20, 30, 40 };
+        double expected = 25;
+
+        // Act
+        double result = Average.CalculateAverage(array);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_CalculateAverage_InputHasNegativeIntegers_ShouldReturnCorrectAverage()
+    {
+        // Arrange
+        int[] array = { -10, -20, -30, -40 };
+        double expected = -25;
+
+        // Act
+        double result = Average.CalculateAverage(array);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_CalculateAverage_InputHasMixedIntegers_ShouldReturnCorrectAverage()
+    {
+        // Arrange
+        int[] array = { -10, 20, -30, 40 };
+        double expected = 5;
+
+        // Act
+        double result = Average.CalculateAverage(array);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
