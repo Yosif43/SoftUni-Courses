b9fab3549184d982760fd9718505d01b6f9e44f2
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-14 18:56:24 +0100

    Create FakeTests.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/07.3.Resources-Exercise-Unit-Testing-Methods-Arrays-Lists/TestApp.UnitTests/FakeTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class FakeTests
+{
+    [Test]
+    public void Test_RemoveStringNumbers_RemovesDigitsFromCharArray()
+    {
+        // Arrange
+        char[] input = { 'a', '1', 'b', '2', 'c' };
+        char[] expected = { 'a', 'b', 'c' };
+
+        // Act
+        var result = Fake.RemoveStringNumbers(input);
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Test_RemoveStringNumbers_NoDigitsInInput_ReturnsSameArray()
+    {
+        // Arrange
+        char[] input = { 'a', 'b', 'c' };
+        char[] expected = { 'a', 'b', 'c' };
+
+        // Act
+        var result = Fake.RemoveStringNumbers(input);
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Test_RemoveStringNumbers_EmptyArray_ReturnsEmptyArray()
+    {
+        // Arrange
+        char[] input = { };
+        char[] expected = { };
+
+        // Act
+        var result = Fake.RemoveStringNumbers(input);
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+}
