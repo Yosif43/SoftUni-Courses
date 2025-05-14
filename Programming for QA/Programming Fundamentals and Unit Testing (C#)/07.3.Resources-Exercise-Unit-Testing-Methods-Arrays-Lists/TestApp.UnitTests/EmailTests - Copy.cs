032c97a9c788d16347315d98e2e6ba1688218eda
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-14 18:56:12 +0100

    Create EmailTests - Copy.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/07.3.Resources-Exercise-Unit-Testing-Methods-Arrays-Lists/TestApp.UnitTests/EmailTests - Copy.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class EmailTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_IsValidEmail_ValidEmail()
+    {
+        // Arrange
+        string validEmail = "test@example.com";
+
+        // Act
+        bool result = Email.IsValidEmail(validEmail);
+
+        // Assert
+        Assert.IsTrue(result);
+
+    }
+
+    [Test]
+    public void Test_IsValidEmail_InvalidEmail()
+    {
+        // Arrange
+        string invalidEmail = "test@.com";
+
+        // Act
+        bool result = Email.IsValidEmail(invalidEmail);
+
+        // Assert
+        Assert.IsFalse(result);
+
+    }
+
+    [Test]
+    public void Test_IsValidEmail_NullInput()
+    {
+        // Arrange
+        string nullEmail = null;
+
+        // Act
+        bool result = Email.IsValidEmail(nullEmail);
+
+        // Assert
+        Assert.IsFalse(result);
+
+    }
+}
