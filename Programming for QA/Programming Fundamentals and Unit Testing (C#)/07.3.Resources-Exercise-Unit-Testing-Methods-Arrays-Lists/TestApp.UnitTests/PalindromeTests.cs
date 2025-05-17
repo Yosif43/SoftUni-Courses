57075f496129e59501408e9da3b28e043039b292
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-17 20:06:40 +0100

    Create PalindromeTests.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/07.3.Resources-Exercise-Unit-Testing-Methods-Arrays-Lists/TestApp.UnitTests/PalindromeTests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+
+using System.Collections.Generic;
+
+namespace TestApp.UnitTests;
+
+public class PalindromeTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_IsPalindrome_ValidPalindrome_ReturnsTrue()
+    {
+        // Arrange
+        List<string> words = new List<string> { "level", "radar", "civic" };
+
+        // Act
+        bool result = Palindrome.IsPalindrome(words);
+
+        // Assert
+        Assert.AreEqual(true, result);
+    }
+
+    // TODO: finish test
+    [Test]
+    public void Test_IsPalindrome_EmptyList_ReturnsTrue()
+    {
+        // Arrange
+        List<string> words = new List<string>();
+
+        // Act
+        bool result = Palindrome.IsPalindrome(words);
+
+        // Assert
+        Assert.AreEqual(true, result);
+    }
+
+    [Test]
+    public void Test_IsPalindrome_SingleWord_ReturnsTrue()
+    {
+        // Arrange
+        List<string> words = new List<string> { "noon" };
+
+        // Act
+        bool result = Palindrome.IsPalindrome(words);
+
+        // Assert
+        Assert.AreEqual(true, result);
+    }
+
+    [Test]
+    public void Test_IsPalindrome_NonPalindrome_ReturnsFalse()
+    {
+        // Arrange
+        List<string> words = new List<string> { "apple", "banana", "carrot" };
+
+        // Act
+        bool result = Palindrome.IsPalindrome(words);
+
+        // Assert
+        Assert.AreEqual(false, result);
+    }
+
+    [Test]
+    public void Test_IsPalindrome_MixedCasePalindrome_ReturnsTrue()
+    {
+        // Arrange
+        List<string> words = new List<string> { "Racecar", "Level", "Civic" };
+
+        // Act
+        bool result = Palindrome.IsPalindrome(words);
+
+        // Assert
+        Assert.AreEqual(true, result);
+    }
+}
