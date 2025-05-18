8d6d97603dad3342b04383529a14cd5f04a5d68a
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-18 20:11:54 +0100

    Create PatternTests.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/07.3.Resources-Exercise-Unit-Testing-Methods-Arrays-Lists/TestApp.UnitTests/PatternTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class PatternTests
+{
+    [Test]
+    public void Test_SortInPattern_SortsIntArrayInPattern_SortsCorrectly()
+    {
+        // Arrange
+        int[] inputArray = new int[] { 5, 3, 2, 8, 5, 2 };
+        int[] expectedOutput = new int[] { 2, 8, 3, 5 };
+
+        // Act
+        var result = Pattern.SortInPattern(inputArray);
+
+        // Assert
+        Assert.AreEqual(expectedOutput, result);
+    }
+
+    [Test]
+    public void Test_SortInPattern_EmptyArray_ReturnsEmptyArray()
+    {
+        // Arrange
+        int[] inputArray = new int[] { };
+        int[] expectedOutput = new int[] { };
+
+        // Act
+        var result = Pattern.SortInPattern(inputArray);
+
+        // Assert
+        Assert.AreEqual(expectedOutput, result);
+    }
+
+    [Test]
+    public void Test_SortInPattern_SingleElementArray_ReturnsSameArray()
+    {
+        // Arrange
+        int[] inputArray = new int[] { 4 };
+        int[] expectedOutput = new int[] { 4 };
+
+        // Act
+        var result = Pattern.SortInPattern(inputArray);
+
+        // Assert
+        Assert.AreEqual(expectedOutput, result);
+    }
+}
