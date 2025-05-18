9575391b86f45bec49f9c39cbc692de0643e138c
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-18 20:12:14 +0100

    Create PrimeFactorTests.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/07.3.Resources-Exercise-Unit-Testing-Methods-Arrays-Lists/TestApp.UnitTests/PrimeFactorTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class PrimeFactorTests
+{
+    [Test]
+    public void Test_FindLargestPrimeFactor_PrimeNumber()
+    {
+        // Arrange
+        long primeNumber = 13;
+        long expected = 13;
+
+        // Act
+        long actual = PrimeFactor.FindLargestPrimeFactor(primeNumber);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+
+    }
+
+    [Test]
+    public void Test_FindLargestPrimeFactor_LargeNumber()
+    {
+        // Arrange
+        long largeNumber = 13195;
+        long expected = 29;
+
+        // Act
+        long actual = PrimeFactor.FindLargestPrimeFactor(largeNumber);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+
+    }
+}
