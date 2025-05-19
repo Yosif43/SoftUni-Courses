cd56ae39699d32ef46c65148deef777d0fcfe1bb
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-19 19:01:59 +0100

    Create ReverserTests.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/07.3.Resources-Exercise-Unit-Testing-Methods-Arrays-Lists/TestApp.UnitTests/ReverserTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class ReverserTests
+{
+    [Test]
+    public void Test_ReverseStrings_WithEmptyArray_ReturnsEmptyArray()
+    {
+        // Arrange
+        string[] inputArray = Array.Empty<string>();
+
+        // Act
+        string[] result = Reverser.ReverseStrings(inputArray);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    // TODO: finish test
+    [Test]
+    public void Test_ReverseStrings_WithSingleString_ReturnsReversedString()
+    {
+        // Arrange
+        string[] inputArray = { "Hello" };
+
+        // Act
+        string[] result = Reverser.ReverseStrings(inputArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(new[] { "olleH" }));
+    }
+
+    [Test]
+    public void Test_ReverseStrings_WithMultipleStrings_ReturnsReversedStrings()
+    {
+        // Arrange
+        string[] inputArray = { "Hello", "World" };
+
+        // Act
+        string[] result = Reverser.ReverseStrings(inputArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(new[] { "olleH", "dlroW" }));
+    }
+
+    [Test]
+    public void Test_ReverseStrings_WithSpecialCharacters_ReturnsReversedSpecialCharacters()
+    {
+        // Arrange
+        string[] inputArray = { "h@ll0", "!world" };
+
+        // Act
+        string[] result = Reverser.ReverseStrings(inputArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(new[] { "0ll@h", "dlrow!" }));
+    }
+}
