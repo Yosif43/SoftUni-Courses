cf74d9d5001b860d953aed0b1ce011c059be190b
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-20 20:32:40 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Arrays - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,32 @@
+﻿string[] days = new string[7];
+days[0] = "Monday";
+days[1] = "Tuesday";
+days[2] = "Wednesday";
+days[3] = "Thursday";
+days[4] = "Friday";
+days[5] = "Saturday";
+days[6] = "Sunday";
+
+string[] moreDays =
+{
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+
+};
+
+int currentDay = int.Parse(Console.ReadLine());
+
+if  (currentDay >= 1 && currentDay <= 7)
+{
+    
+    Console.WriteLine(days[currentDay - 1]);
+}
+else
+{
+    Console.WriteLine("Invalid day!");
+}
