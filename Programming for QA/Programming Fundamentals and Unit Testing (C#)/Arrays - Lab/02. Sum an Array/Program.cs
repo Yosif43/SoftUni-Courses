afae28ca7fb4e58a3318e6aaedfa372cde2793fd
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-20 20:33:11 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Arrays - Lab/02. Sum an Array/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+int arr = 0;
+
+for  (int i = 0; i < numbers.Length; i++)
+{
+    arr += numbers[i];
+    
+}
+
+Console.WriteLine(arr);
