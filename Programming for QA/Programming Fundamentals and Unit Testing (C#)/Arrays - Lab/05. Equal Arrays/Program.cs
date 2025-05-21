6ffca9c411d22fe7632c1fe994af0c40460f737b
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-21 20:11:12 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Arrays - Lab/05. Equal Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] array1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+int[] array2 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+bool areIdentical = true;
+for (int i = 0; i < array1.Length; i++)
+{
+    if (array1[i] != array2[i])
+    {
+        areIdentical = false;
+        break;
+    }
+}
+
+if (areIdentical)
+{
+    Console.WriteLine("Arrays are identical.");
+}
+else
+{
+    Console.WriteLine("Arrays are not identical.");
+}
