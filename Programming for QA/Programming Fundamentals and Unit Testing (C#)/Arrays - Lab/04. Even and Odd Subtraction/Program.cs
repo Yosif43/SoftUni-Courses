3c08f1efb1db8958f55011abc5e3425dce6501f3
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-21 20:10:54 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Arrays - Lab/04. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+int sumEven = numbers.Where(x => x % 2 == 0).Sum();
+
+int sumOdd = numbers.Where(x => x % 2 != 0).Sum();
+
+int difference = sumEven - sumOdd;
+
+Console.WriteLine(difference);
