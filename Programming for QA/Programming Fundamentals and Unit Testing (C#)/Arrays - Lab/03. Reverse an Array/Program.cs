9db34c8d7032e7ff84a523abe75d462c047678a7
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-21 20:10:30 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Arrays - Lab/03. Reverse an Array/Program.cs
@@ -0,0 +1,12 @@
+﻿int N = int.Parse(Console.ReadLine());
+
+int[] numbers = new int[N];
+
+for (int i = 0; i < N; i++)
+{
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+Array.Reverse(numbers);
+
+Console.WriteLine(string.Join(" ", numbers));
