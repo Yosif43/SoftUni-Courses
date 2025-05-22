a271125ff06bff32dfc7d7e3e132f757eab4c93d
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-22 20:30:20 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Arrays and Lists - Exercise/01. Gauss' Trick/Program.cs
@@ -0,0 +1,18 @@
+﻿List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+int i = 0;
+int j = numbers.Count - 1;
+
+while (i <= j)
+{
+    if (i == j)
+    {
+        Console.Write($"{numbers[i]} ");
+    }
+    else
+    {
+        Console.Write($"{numbers[i] + numbers[j]} ");
+    }
+    i++;
+    j--;
+}
