2bd7872e865aba263c050c25d92d34c5c14ebb44
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-22 20:28:29 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Arrays - Lab/06. Common Elements/Program.cs
@@ -0,0 +1,7 @@
+﻿int[] array1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+int[] array2 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+int[] commonElements = array1.Intersect(array2).ToArray();
+
+Console.WriteLine(string.Join(" ", commonElements));
