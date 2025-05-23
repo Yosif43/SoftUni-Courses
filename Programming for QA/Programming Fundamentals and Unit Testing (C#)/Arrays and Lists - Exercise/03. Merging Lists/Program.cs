8ddca05bd5a6aa5e4a9843e7c9baec75612597aa
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-23 21:57:40 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Arrays and Lists - Exercise/03. Merging Lists/Program.cs
@@ -0,0 +1,20 @@
+﻿List<int> firstSequence = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+List<int> secondSequence = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+List<int> mergedList = new List<int>();
+
+int longerCount = Math.Max(firstSequence.Count, secondSequence.Count);
+
+for (int i = 0; i < longerCount; i++)
+{
+    if (i < firstSequence.Count)
+    {
+        mergedList.Add(firstSequence[i]);
+    }
+    if (i < secondSequence.Count)
+    {
+        mergedList.Add(secondSequence[i]);
+    }
+}
+
+Console.WriteLine(string.Join(" ", mergedList));
