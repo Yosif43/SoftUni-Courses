30b89567ce9e9218586a030182e969bdf6c48f7b
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-24 18:58:58 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Arrays and Lists - Exercise/06. Cards Game/Program.cs
@@ -0,0 +1,34 @@
+﻿List<int> firstPlayerCards = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+List<int> secondPlayerCards = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+while (firstPlayerCards.Count > 0 && secondPlayerCards.Count > 0)
+{
+    int firstPlayerCard = firstPlayerCards[0];
+    int secondPlayerCard = secondPlayerCards[0];
+
+    if (firstPlayerCard > secondPlayerCard)
+    {
+        firstPlayerCards.Add(firstPlayerCard);
+        firstPlayerCards.Add(secondPlayerCard);
+    }
+    else if (secondPlayerCard > firstPlayerCard)
+    {
+        secondPlayerCards.Add(secondPlayerCard);
+        secondPlayerCards.Add(firstPlayerCard);
+    }
+
+    firstPlayerCards.RemoveAt(0);
+    secondPlayerCards.RemoveAt(0);
+
+}
+
+if (firstPlayerCards.Count > 0)
+{
+    int sum = firstPlayerCards.Sum();
+    Console.WriteLine($"First player wins! Sum: {sum}");
+}
+else
+{
+    int sum = secondPlayerCards.Sum();
+    Console.WriteLine($"Second player wins! Sum: {sum}");
+}
