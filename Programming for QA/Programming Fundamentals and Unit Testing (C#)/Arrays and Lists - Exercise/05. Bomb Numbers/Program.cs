58154cbcf7941dfa5c3775fe5ce470b21229fc17
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-24 18:56:29 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Arrays and Lists - Exercise/05. Bomb Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+int[] bombInfo = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+int bombNumber = bombInfo[0];
+int power = bombInfo[1];
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    if (numbers[i] == bombNumber)
+    {
+        int leftIndex = Math.Max(i - power, 0);
+        int rightIndex = Math.Min(i + power, numbers.Count - 1);
+
+        numbers.RemoveRange(leftIndex, rightIndex - leftIndex + 1);
+
+        i = leftIndex - 1;
+    }
+}
+
+int sum = numbers.Sum();
+Console.WriteLine(sum);
