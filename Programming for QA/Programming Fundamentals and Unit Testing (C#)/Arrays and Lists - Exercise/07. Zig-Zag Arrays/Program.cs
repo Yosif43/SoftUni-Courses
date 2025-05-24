f856f6689e31bdf0027458417f8b546c66959584
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-24 18:59:22 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Arrays and Lists - Exercise/07. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿int N = int.Parse(Console.ReadLine());
+
+int[] array1 = new int[N];
+int[] array2 = new int[N];
+
+for (int i = 0; i < N; i++)
+{
+    string[] input = Console.ReadLine().Split(' ');
+    int firstNumber = int.Parse(input[0]);
+    int secondNumber = int.Parse(input[1]);
+
+    if (i % 2 == 0)
+    {
+        array1[i] = firstNumber;
+        array2[i] = secondNumber;
+    }
+    else
+    {
+        array1[i] = secondNumber;
+        array2[i] = firstNumber;
+    }
+}
+
+Console.WriteLine(string.Join(" ", array1));
+Console.WriteLine(string.Join(" ", array2));
