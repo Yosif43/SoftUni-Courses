f037e9a5e95e18d0d3b4aeda8c41bd0ef0314653
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-25 19:20:34 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Arrays and Lists - Exercise/09. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,29 @@
+﻿List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+int bestStart = 0;
+int bestLength = 1;
+int currentStart = 0;
+int currentLength = 1;
+
+for (int i = 1; i < numbers.Count; i++)
+{
+    if (numbers[i] == numbers[i - 1])
+    {
+        currentLength++;
+        if (currentLength > bestLength)
+        {
+            bestLength = currentLength;
+            bestStart = currentStart;
+        }
+    }
+    else
+    {
+        currentLength = 1;
+        currentStart = i;
+    }
+}
+
+for (int i = 0; i < bestLength; i++)
+{
+    Console.Write(numbers[bestStart] + " ");
+}
