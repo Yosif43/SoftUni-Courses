63e34562348a2484acbdd42d0061ebe5ef70029b
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-25 19:20:56 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Arrays and Lists - Exercise/10. Top Integers/Program.cs
@@ -0,0 +1,23 @@
+﻿List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+List<int> topIntegers = new List<int>();
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    bool isTopInteger = true;
+    int currentNumber = numbers[i];
+    for (int j = i + 1; j < numbers.Count; j++)
+    {
+        if (currentNumber <= numbers[j])
+        {
+            isTopInteger = false;
+            break;
+        }
+    }
+
+    if (isTopInteger)
+    {
+        topIntegers.Add(currentNumber);
+    }
+}
+
+Console.WriteLine(string.Join(" ", topIntegers));
