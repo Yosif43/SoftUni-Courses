11d373d0387eaa6062fdc3cbef02bb6d72e45d72
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-26 18:54:39 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Complex Conditional Statements - Lab/01. Market Place/Program.cs
@@ -0,0 +1,40 @@
+﻿string product = Console.ReadLine();
+string day = Console.ReadLine();
+
+if (day == "Weekday")
+{
+    if (product == "Banana") {
+        double banana = 2.50;
+        Console.WriteLine($"{banana:F2}");
+    }
+    else if (product == "Apple")
+    {
+        double apple = 1.30;
+        Console.WriteLine($"{apple:F2}");
+    }
+    else if(product == "Kiwi")
+    {
+        double kiwi = 2.20;
+        Console.WriteLine($"{kiwi:F2}");    
+    }
+    
+}
+else if (day == "Weekend")
+{
+    if (product == "Banana")
+    {
+        double banana = 2.70;
+        Console.WriteLine($"{banana:F2}");
+    }
+    else if (product == "Apple")
+    {
+        double apple = 1.60;
+        Console.WriteLine($"{apple:F2}");
+    }
+    else if (product == "Kiwi")
+    {
+        double kiwi = 3.00;
+        Console.WriteLine($"{kiwi:F2}");
+    }
+}
+    
