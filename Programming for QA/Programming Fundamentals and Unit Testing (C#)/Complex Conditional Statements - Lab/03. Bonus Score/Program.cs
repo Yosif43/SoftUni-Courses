335a35ab6fed6cffdbc93ba1b7c2526f4ae18788
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-26 18:55:20 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Complex Conditional Statements - Lab/03. Bonus Score/Program.cs
@@ -0,0 +1,17 @@
+﻿int points = int.Parse(Console.ReadLine());
+
+if  (points >= 0 && points <= 3)
+{
+    double result = points + 5;
+    Console.WriteLine(result);
+}
+else if (points >= 4 && points <= 6)
+{
+    double result = points + 15;
+    Console.WriteLine(result);
+}
+else if (points >= 7 && points <= 9) 
+{
+    double result = points + 20;
+    Console.WriteLine(result);
+}
