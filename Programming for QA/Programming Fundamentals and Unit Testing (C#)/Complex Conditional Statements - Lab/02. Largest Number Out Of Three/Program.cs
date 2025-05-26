9b888077324e9e1ac91da39800d39794ea83168a
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-26 18:54:59 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Complex Conditional Statements - Lab/02. Largest Number Out Of Three/Program.cs
@@ -0,0 +1,12 @@
+﻿int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+int number3 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+    if (number1 > number3) Console.WriteLine(number1);
+    else Console.WriteLine(number3);
+}
+else 
+    if (number2 > number3) Console.WriteLine(number2);
+    else Console.WriteLine(number3);
