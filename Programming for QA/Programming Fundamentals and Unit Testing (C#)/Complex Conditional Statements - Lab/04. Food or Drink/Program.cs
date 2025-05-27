d52753d235f316c78eef520f635c4884b8c0ce49
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-27 20:01:01 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Complex Conditional Statements - Lab/04. Food or Drink/Program.cs
@@ -0,0 +1,7 @@
+﻿string product = Console.ReadLine();
+
+if (product == "curry" || product == "noodles" || product == "sushi" || product == "spaghetti" || product == "bread") Console.WriteLine("food");
+
+else if (product == "tea" || product == "water" || product == "coffee" || product == "juice") Console.WriteLine("drink");
+
+else Console.WriteLine("unknown");
