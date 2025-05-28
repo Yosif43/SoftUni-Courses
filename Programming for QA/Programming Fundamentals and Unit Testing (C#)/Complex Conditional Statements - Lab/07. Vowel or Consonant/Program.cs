b17845bf38bec88a92e5fb35c9b2b9e663c9a6b4
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-28 19:37:10 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Complex Conditional Statements - Lab/07. Vowel or Consonant/Program.cs
@@ -0,0 +1,24 @@
+﻿string vowel = Console.ReadLine();
+if (vowel == "A" || vowel == "a" || vowel == "E" || vowel == "e" || vowel == "I" || vowel == "i" || vowel == "O" || vowel == "o" || vowel == "U" || vowel == "u") Console.WriteLine("Vowel");
+else Console.WriteLine("Consonant");
+
+// or 
+
+switch (vowel)
+{
+    case "a":
+    case "A":
+    case "e":
+    case "E":
+    case "I":
+    case "i":
+    case "O":
+    case "o":
+    case "U":
+    case "u":
+        Console.WriteLine("Vowel");
+        break;
+    default: Console.WriteLine("Consonant");
+        break;
+
+}
