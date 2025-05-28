b56c01ad8aabcbdaf4c8c46f76762877f6c7ee21
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-28 19:36:49 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Complex Conditional Statements - Lab/06. Day Of Week/Program.cs
@@ -0,0 +1,10 @@
+﻿int dayofWeek = int.Parse(Console.ReadLine());
+
+if (dayofWeek == 1) Console.WriteLine("Monday");
+else if (dayofWeek == 2) Console.WriteLine("Tuesday");
+else if (dayofWeek == 3) Console.WriteLine("Wednesday");
+else if (dayofWeek == 4) Console.WriteLine("Thursday");
+else if (dayofWeek == 5) Console.WriteLine("Friday");
+else if (dayofWeek == 6) Console.WriteLine("Saturday");
+else if (dayofWeek == 7) Console.WriteLine("Sunday");
+else Console.WriteLine("Error");
