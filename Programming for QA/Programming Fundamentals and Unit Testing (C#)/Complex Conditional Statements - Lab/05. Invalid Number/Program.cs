12605f64c2b47fee2209c78acdc50a6cd94b1f31
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-28 19:36:25 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Complex Conditional Statements - Lab/05. Invalid Number/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+if (number == 0 || number >= 100 && number <= 200);
+else Console.WriteLine("invalid");
+
+// or
+
+bool isValid = (number == 0 || number >= 100 && number <= 200);
+if (!isValid)
+{
+    Console.WriteLine("invalid");
+}
+
+// or 
+if (!(number == 0 || number >= 100) && number <= 200) Console.WriteLine("invalid");
