a2179d4ab7159488bc2d57210c6ed5428b4d9024
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-29 19:23:40 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Exam/01. Count Letters/Program.cs
@@ -0,0 +1,25 @@
+﻿string text = Console.ReadLine();
+
+int upperCaseCount = 0;
+int lowerCaseCount = 0;
+int spaceCount = 0;
+
+foreach (char c in text)
+{
+    if (char.IsUpper(c))
+    {
+        upperCaseCount++;
+    }
+    else if (char.IsLower(c))
+    {
+        lowerCaseCount++;
+    }
+    else if (c == ' ')
+    {
+        spaceCount++;
+    }
+}
+
+Console.WriteLine(upperCaseCount);
+Console.WriteLine(lowerCaseCount);
+Console.WriteLine(spaceCount);
