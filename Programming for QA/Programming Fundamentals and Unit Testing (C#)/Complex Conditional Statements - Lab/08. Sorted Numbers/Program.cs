7d94fff61f5d9c86b9b234d12ad3e4ace08ae35c
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-29 19:22:33 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Complex Conditional Statements - Lab/08. Sorted Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2  = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 < num2 && num2 < num3)
+{
+    Console.WriteLine("Ascending");
+}
+else if (num1 > num2 && num2 > num3)
+{
+    Console.WriteLine("Descending");
+}
+else
+{
+    Console.WriteLine("Not sorted");
+}
