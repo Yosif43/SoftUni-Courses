358ec90f3ea364bf08c2dee2746566fe17bea042
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-29 19:23:03 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Complex Conditional Statements - Lab/09. Cinema/Program.cs
@@ -0,0 +1,22 @@
+﻿string typeOfMovie = Console.ReadLine();
+int Rows = int.Parse(Console.ReadLine());
+int Seats = int.Parse(Console.ReadLine());
+
+if (typeOfMovie == "Premiere")
+{
+    int totalSeats = Rows * Seats;
+    double totalPrice = totalSeats * 12.00;
+    Console.WriteLine($"{totalPrice:F2}");
+}
+else if (typeOfMovie == "Normal")
+{
+    int totalSeats = Rows * Seats;
+    double totalPrice = totalSeats * 7.50;
+    Console.WriteLine($"{totalPrice:F2}");
+}
+else if (typeOfMovie == "Discount")
+{
+    int totalSeats = Rows * Seats;
+    double totalPrice = totalSeats * 5.00;
+    Console.WriteLine($"{totalPrice:F2}");
+}
