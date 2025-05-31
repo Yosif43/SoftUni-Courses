670f94d7d34c19593e4233798691abf807157191
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-05-31 10:41:34 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Exam/02. Max - Min in Range/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+int startIndex = int.Parse(Console.ReadLine());
+int endIndex = int.Parse(Console.ReadLine());
+
+if (startIndex < 0 || endIndex >= numbers.Length || startIndex > endIndex)
+{
+    Console.WriteLine("Invalid range.");
+    return;
+}
+
+int maxNumber = numbers[startIndex];
+int minNumber = numbers[startIndex];
+for (int i = startIndex; i <= endIndex; i++)
+{
+    if (numbers[i] > maxNumber) maxNumber = numbers[i];
+    if (numbers[i] < minNumber) minNumber = numbers[i];
+}
+
+int sum = maxNumber + minNumber;
+
+Console.WriteLine(sum);
