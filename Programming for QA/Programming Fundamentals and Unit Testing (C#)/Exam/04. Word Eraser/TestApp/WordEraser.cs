369a060b8de658f0f3d796e4aa0cdfe3062ccb90
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-01 22:33:41 +0100

    Create WordEraser.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Exam/04. Word Eraser/TestApp/WordEraser.cs
@@ -0,0 +1,29 @@
+﻿namespace TestApp;
+
+public class WordEraser
+{
+    public string Erase(List<string>? words, string? wordToErase)
+    {
+        if (words == null || words.Count == 0)
+        {
+            return string.Empty;
+        }
+
+        if (string.IsNullOrEmpty(wordToErase))
+        {
+            return string.Join(" ", words);
+        }
+
+        for (int i = 0; i < words.Count; i++)
+        {
+            if (words[i] == wordToErase)
+            {
+                words.Remove(wordToErase);
+                i--;
+            }
+        }
+
+        return string.Join(" ", words);
+    }
+}
+
