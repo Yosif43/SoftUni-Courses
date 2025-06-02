e631048daa0a52c099662c9c387a3c52c349081d
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-02 19:10:08 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Exam Preparation II/01. Magic Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+bool isNoMagicNumbers = true;
+
+for (int i = 1; i <= n; i++)
+{
+    int currentNum = i;
+    int sumDigits = 0;
+    bool isAllDigitsPrime = true;
+
+    while (currentNum > 0)
+    {
+        int digit = currentNum % 10;
+        currentNum /= 10;
+
+        sumDigits += digit;
+
+        if (digit != 2 && digit != 3 && digit != 5 && digit != 7)
+        {
+            isAllDigitsPrime = false;
+        }
+    }
+    if (isAllDigitsPrime && sumDigits % 2 == 0)
+    {
+        Console.Write(i + " ");
+        isNoMagicNumbers = false;
+    }
+}
+
+if (isNoMagicNumbers)
+{
+    Console.WriteLine("no");
+}
