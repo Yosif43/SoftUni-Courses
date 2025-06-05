d5e4fe8652460066801b6abe617663df8d97782e
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-05 19:20:01 +0100

    Create ExtractFileTests.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Exam Preparation III/03-Extract-File-Resources/TestApp.Tests/ExtractFileTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestApp.Tests;
+
+public class ExtractFileTests
+{
+    [Test]
+    public void Test_GetFile_NullPath_ThrowsArgumentNullException()
+    {
+        // Arrange
+        string? nullPath = null;
+        //Act
+
+        //Assert
+        Assert.Throws<ArgumentNullException>(() =>
+        {
+            ExtractFile.GetFile(nullPath);
+        });
+    }
+
+    [Test]
+    public void Test_GetFile_EmptyPath_ThrowsArgumentNullException()
+    {
+        // Arrange
+        string emptyPath = string.Empty;
+        //Act
+
+        //Assert
+        Assert.Throws<ArgumentNullException>(() =>
+        {
+            ExtractFile.GetFile(emptyPath);
+        });
+    }
+
+    [Test]
+    public void Test_GetFile_ValidPath_ReturnsFileNameAndExtension()
+    {
+        // Arrange
+        string validPath = @"C:\Users\Yoo\Desktop\03-Extract-File-Resources\ExamPreparation.sln";
+        string expectedResult = "File name: ExamPreparation\nFile extension: sln";
+        //Act
+        string actualResult = ExtractFile.GetFile(validPath);
+        //Assert
+        Assert.AreEqual(expectedResult, actualResult);
+
+    }
+
+    [Test]
+    public void Test_GetFile_PathWithNoExtension_ReturnsFileNameOnly()
+    {
+        // Arrange
+        string validPath = @"C:\Users\Yoo\Desktop\03-Extract-File-Resources\ExamPreparation";
+        string expectedResult = $"File name: ExamPreparation";
+        //Act
+        string actualResult = ExtractFile.GetFile(validPath);
+        //Assert
+        Assert.AreEqual (expectedResult, actualResult);
+    }
+
+    [Test]
+    public void Test_GetFile_PathWithSpecialCharacters_ReturnsFileNameAndExtension()
+    {
+        // Arrange
+        string validPathWithSpecialCharacters = @"C:\Users\Yoo\Desktop\03-Extract-File-Resources\readMe.pdf.txt";
+        string expectedResult = "File name: readMe\nFile extension: pdf";
+        //Act
+        string actualResult = ExtractFile.GetFile(validPathWithSpecialCharacters);
+        //Assert
+        Assert.AreEqual (expectedResult, actualResult);
+    }
+}
