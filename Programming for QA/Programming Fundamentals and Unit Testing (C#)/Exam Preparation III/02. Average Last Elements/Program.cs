61400116a822216dcb787b0ac5eea8d4da853b98
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-05 19:19:15 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Exam Preparation III/02. Average Last Elements/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int elements = int.Parse(Console.ReadLine());
+int arrLenght = numbers.Length;
+int sum = 0;
+
+for (int i = 0; i < elements; i++)
+{
+    int currentNumber = numbers[arrLenght - 1 - i];
+    sum += currentNumber;
+}
+double lastElementAvg = (double)sum / elements;
+Console.WriteLine($"{lastElementAvg:F2}");
