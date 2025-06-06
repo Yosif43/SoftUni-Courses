7f13403cf5abf2c9dd808a3726580dbd1fe3eff6
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-06 16:05:44 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Exam Preparation-I/01. Sum Factorial Even Digits/Program.cs
@@ -0,0 +1,23 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+while (number > 0)
+{
+    int digit = number % 10;
+    number /= 10;
+
+    if (digit % 2 == 0)
+    {
+        int factorial = 1;
+
+        for (int i = 1;  i <= digit; i++)
+        {
+            factorial *= i;
+        }
+        sum += factorial;
+    }
+}
+
+
+Console.WriteLine(sum);
