ff2f1975d31492f28478be9e3cd71f9f97150c82
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-07 10:46:20 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Exam Preparation-I/02. Middle Elements/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int middle = numbers.Length / 2;
+
+int firstNumber = numbers[middle - 1];
+int secondNumber = numbers[middle];
+double sum = (firstNumber + secondNumber) / 2.0;
+Console.WriteLine($"{sum:F2}");
