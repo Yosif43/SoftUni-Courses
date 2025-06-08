0370ce9c2ce933fb7615b3176527141383f925db
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-08 19:00:08 +0100

    Create FoldSumTests.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Exam Preparation-I/04-FoldArray-Resources/TestApp.Tests/FoldSumTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class FoldSumTests
+{
+    [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, "5 5 13 13")]
+    [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }, "7 7 7 19 19 19")]
+    [TestCase(new int[] { 1, 1, 1, 1 }, "2 2")]
+    [TestCase(new int[] { 1, -2, 3, -4, 5, -6, 7, -8 }, "1 -3 -3 1")]
+    [TestCase(new int[] { -1, -2, -3, -4, }, "-3 -7")]
+    public void Test_FoldArray_ReturnsCorrectString(int[] arr, string expected)
+    {
+
+        string actual = FoldSum.FoldArray(arr);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
