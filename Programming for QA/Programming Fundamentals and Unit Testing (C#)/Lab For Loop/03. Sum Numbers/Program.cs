ec53e933fe7d7b762c40fadf57fe5f5af49bbc56
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-10 20:39:21 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Lab For Loop/03. Sum Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿double num1 = double.Parse(Console.ReadLine());
+
+double sum = 0;
+
+for  (double i = 1; i <= num1; i++)
+{
+    double number = double.Parse(Console.ReadLine());
+    sum += number;
+}
+
+Console.WriteLine(sum);
