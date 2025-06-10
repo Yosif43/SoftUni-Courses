acc52c6ee2f18e85da23a381450c85b4664a0f48
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-10 20:39:01 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Lab For Loop/02. First N Numbers Sum/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+for (int i = 1; i < number; i++)
+{
+    sum += i;
+    Console.Write($"{i}+");
+}
+sum += number;
+Console.WriteLine($"{number}={sum}");
