948fe0207403dc8f7ca85e21441529ed0cddb02c
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-15 22:44:40 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Lab Methods/01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+PrintNumber(num);
+
+static void PrintNumber(int number)
+{
+    if (number == 0)
+    {
+        Console.WriteLine($"The number {number} is zero.");
+    }
+    else if (number > 0) {
+        Console.WriteLine($"The number {number} is positive.");
+    }
+    else
+    {
+        Console.WriteLine($"The number {number} is negative.");
+    }
+}
