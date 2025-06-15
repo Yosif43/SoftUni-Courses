7569399a8a015be7a22f6039cd5e7386d77908e8
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-15 22:45:20 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Lab Methods/03. Printing Triangle/Program.cs
@@ -0,0 +1,25 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+PrintTriangle(n);
+
+
+static void PrintLine(int start, int end)
+{
+    for (int i = start; i <= end; i++)
+    {
+        Console.Write($"{i} ");
+    }
+    Console.WriteLine();
+}
+
+static void PrintTriangle(int size)
+{
+    for (int i = 1; i < size; i++)
+    {
+        PrintLine(1,i);
+    }
+    for (int i = size; i >= 1; i--)
+    {
+        PrintLine(1, i);
+    }
+}
