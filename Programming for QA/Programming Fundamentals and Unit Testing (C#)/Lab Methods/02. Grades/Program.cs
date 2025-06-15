f4707111d22c17cb85a30f453cc76f41819e193e
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-15 22:45:05 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Lab Methods/02. Grades/Program.cs
@@ -0,0 +1,29 @@
+﻿double grades = double.Parse(Console.ReadLine());
+
+
+PrintGradeInWords(grades);
+
+static void PrintGradeInWords(double grade)
+{
+    if (grade < 3.00)
+    {
+        Console.WriteLine("Fail");
+    }
+    else if (grade < 3.50)
+    {
+        Console.WriteLine("Average");
+    }
+    else if (grade < 4.50)
+    {
+        Console.WriteLine("Good");
+    }
+    else if (grade < 5.50)
+    {
+        Console.WriteLine("Very good");
+    }
+
+    else
+    {
+        Console.WriteLine("Excellent");
+    }
+}
