e5fe57d64bf2e4acf61e5c82221f70dfb3426ece
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-16 21:34:58 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Lab Methods/06. Math Power/Program.cs
@@ -0,0 +1,14 @@
+﻿int baseNumber = int.Parse(Console.ReadLine());
+int power = int.Parse(Console.ReadLine());
+
+Console.WriteLine(MathPow(baseNumber, power));
+
+static int MathPow(int baseNum, int powers)
+{
+    int output = baseNum;
+    for (int i = 2; i <= powers; i++)
+    {
+        output *= baseNum;
+    }
+    return output;
+}
