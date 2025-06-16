8ec09f4efc061beefe91abb4ccab2c8bf716be09
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-16 21:34:36 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Lab Methods/05. Repeat String/Program.cs
@@ -0,0 +1,16 @@
+﻿string text = Console.ReadLine();
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(RepeatString(text, n));
+static string RepeatString(string input, int count)
+{
+    string output = "";
+    for (int i = 1; i <= count; i++)
+    {
+        output += input;
+    }
+
+
+
+    return output;
+}
