4aca67824759107bb0303dabc10d98d7253e96ef
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-16 21:34:09 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Lab Methods/04. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,11 @@
+﻿int w = int.Parse(Console.ReadLine());
+int l = int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetRectangleArea(w, l));
+
+
+static int GetRectangleArea(int width, int lenght)
+{
+    int area = width * lenght;
+    return area;
+}
