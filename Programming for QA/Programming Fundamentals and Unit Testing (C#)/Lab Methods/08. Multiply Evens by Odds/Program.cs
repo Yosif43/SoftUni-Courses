056e6d449d37f395a7c74e4a553fb012c339e49f
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-17 21:37:00 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Lab Methods/08. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,40 @@
+﻿int n = Math.Abs(int.Parse(Console.ReadLine()));
+
+Console.WriteLine(GetMultipleOfEvenAndOdds(n));
+
+static int GetSumOfEvenDigits(int number)
+{
+    int evenSum = 0;
+
+    while (number > 0)
+    {
+        int digit = number % 10;
+        number /= 10;
+        if (digit % 2 == 0)
+        {
+            evenSum += digit;
+        }
+    }
+    return evenSum;
+}
+
+static int GetSumOfOddDigits(int number)
+{
+    int oddSum = 0;
+
+    while (number > 0)
+    {
+        int digit = number % 10;
+        number /= 10;
+        if (digit % 2 != 0)
+        {
+            oddSum += digit;
+        }
+    }
+    return oddSum;
+}
+
+static int GetMultipleOfEvenAndOdds(int number)
+{
+    return GetSumOfEvenDigits(number) * GetSumOfOddDigits(number);
+}
