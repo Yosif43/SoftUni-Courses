06a5e9dde6428e199ebe01da8f3037ece6f7117a
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-17 21:37:30 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Lab Methods/09. Orders/Program.cs
@@ -0,0 +1,26 @@
+﻿string product = Console.ReadLine();
+int quantity = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{GetFinalPrice(product, quantity):F2}");
+static double GetFinalPrice(string p, int q)
+{
+    double singlePrice = 0;
+    if (p == "coffee")
+    {
+        singlePrice = 1.50;
+    }
+    else if (p == "water")
+    {
+        singlePrice = 1.00;
+    }
+    else if (p == "coke")
+    {
+        singlePrice = 1.40;
+    }
+    else if (p == "snacks")
+    {
+        singlePrice = 2.00;
+    }
+
+    return singlePrice * q;
+}
