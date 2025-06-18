32e678bc46529c863454a8763c39ca0f6754070f
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-18 22:07:45 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Lists - Lab/01. Change List/Program.cs
@@ -0,0 +1,24 @@
+﻿List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+string command;
+while ((command = Console.ReadLine()) != "end")
+{
+    string[] tokens = command.Split(' ');
+    switch (tokens[0])
+    {
+        case "Delete":
+            int elementToDelete = int.Parse(tokens[1]);
+            numbers.RemoveAll(x => x == elementToDelete);
+            break;
+        case "Insert":
+            int elementToInsert = int.Parse(tokens[1]);
+            int position = int.Parse(tokens[2]);
+            if (position >= 0 && position <= numbers.Count) // Check if position is valid
+            {
+                numbers.Insert(position, elementToInsert);
+            }
+            break;
+    }
+}
+
+Console.WriteLine(string.Join(" ", numbers));
