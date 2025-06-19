9bf7b038fc6d2a90ab56cccc927147172b2aff88
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-19 20:25:23 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Lists - Lab/04. List Manipulation Basics/Program.cs
@@ -0,0 +1,31 @@
+﻿List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+string command;
+while ((command = Console.ReadLine()) != "end")
+{
+    string[] tokens = command.Split(' ');
+    int number, index;
+
+    switch (tokens[0])
+    {
+        case "Add":
+            number = int.Parse(tokens[1]);
+            numbers.Add(number);
+            break;
+        case "Remove":
+            number = int.Parse(tokens[1]);
+            numbers.Remove(number);
+            break;
+        case "RemoveAt":
+            index = int.Parse(tokens[1]);
+            numbers.RemoveAt(index);
+            break;
+        case "Insert":
+            number = int.Parse(tokens[1]);
+            index = int.Parse(tokens[2]);
+            numbers.Insert(index, number);
+            break;
+    }
+}
+
+Console.WriteLine(string.Join(" ", numbers));
