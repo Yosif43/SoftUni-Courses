932c8c7fb4b6437893c56b5a91ca194139dcc6c9
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-19 20:26:30 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Loops - Exercise/01. Power Of Number/Program.cs
@@ -0,0 +1,9 @@
+﻿int numberN = int.Parse(Console.ReadLine());
+int numberP = int.Parse(Console.ReadLine());
+
+int sum = 1;
+for  (int i = 0; i < numberP; i++)
+{
+    sum *= numberN;
+}
+Console.WriteLine(sum);
