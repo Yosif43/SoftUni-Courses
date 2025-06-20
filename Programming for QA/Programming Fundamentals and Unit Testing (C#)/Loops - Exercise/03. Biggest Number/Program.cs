7ff130cf6e6d3f533984a4da6fd9af9d3db8ec13
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-20 21:21:24 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Loops - Exercise/03. Biggest Number/Program.cs
@@ -0,0 +1,13 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+int maximumNumber = 0;
+for (int i = 0; i < num; i++)
+{
+    int currentNumber = int.Parse(Console.ReadLine());
+    if (currentNumber > maximumNumber)
+    {
+        maximumNumber = currentNumber;
+    }
+}
+
+Console.WriteLine(maximumNumber);
