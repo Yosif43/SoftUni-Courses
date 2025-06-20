46f2fb34e29fdf3e49159a5b295e84be50e15c67
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-20 21:21:39 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Loops - Exercise/04. Vowel Sum/Program.cs
@@ -0,0 +1,18 @@
+﻿int N = int.Parse(Console.ReadLine());
+int sum = 0;
+
+for (int i = 0; i < N; i++)
+{
+    char character = char.Parse(Console.ReadLine().ToLower());
+
+    switch (character)
+    {
+        case 'a': sum += 1; break;
+        case 'e': sum += 2; break;
+        case 'i': sum += 3; break;
+        case 'o': sum += 4; break;
+        case 'u': sum += 5; break;
+    }
+}
+
+Console.WriteLine($"{sum}"); 
