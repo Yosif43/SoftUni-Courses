97023895456b7ff43b8eec08086185fd78a53460
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-21 09:50:07 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Loops - Exercise/07. Special Bonus/Program.cs
@@ -0,0 +1,17 @@
+﻿int stopNumber = int.Parse(Console.ReadLine());
+int number = 0;
+int previousNumber = 0;
+
+while (true)
+{
+    number = int.Parse(Console.ReadLine());
+
+    if (number == stopNumber)
+    {
+
+        double increasedValue = previousNumber * 1.2;
+        Console.WriteLine($"{increasedValue}");
+        break;
+    }
+    previousNumber = number;
+}
