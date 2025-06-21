677726587ec529dcdc11ed501e42646a9406215e
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-21 09:49:19 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Loops - Exercise/06. Special Number/Program.cs
@@ -0,0 +1,24 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+int tempNumber = number;
+bool isSpecialNumber = true;
+while (tempNumber > 0)
+{
+    int currentLastDigit = tempNumber % 10;
+    tempNumber /= 10;
+
+    if (number % currentLastDigit != 0)
+    {
+        isSpecialNumber = false;
+        break;
+    }
+}
+
+if (isSpecialNumber)
+{
+    Console.WriteLine($"{number} is special");
+}
+else
+{
+    Console.WriteLine($"{number} is not special");
+}
