9992b315cf8dc406237fdd7b8360a8ee45bd2fa7
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-21 09:48:41 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Loops - Exercise/05. Division to 2, 3 and 4/Program.cs
@@ -0,0 +1,32 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int numsDividedBy2Mod0Count = 0;
+int numsDividedBy3Mod0Count = 0;
+int numsDividedBy4Mod0Count = 0;
+for (int i = 0; i < n; i++)
+{
+    int currentNumber = int.Parse(Console.ReadLine());
+
+    if (currentNumber % 2 == 0)
+    {
+        numsDividedBy2Mod0Count++;
+    }
+
+    if (currentNumber % 3 == 0)
+    {
+        numsDividedBy3Mod0Count++;
+    }
+
+    if (currentNumber % 4 == 0)
+    {
+        numsDividedBy4Mod0Count++;
+    }
+}
+
+double numsDividedBy2Mod0Percentage = (double)numsDividedBy2Mod0Count / n * 100;
+double numsDividedBy3Mod0Percentage = (double)numsDividedBy3Mod0Count / n * 100;
+double numsDividedBy4Mod0Percentage = (double)numsDividedBy4Mod0Count / n * 100;
+
+Console.WriteLine($"{numsDividedBy2Mod0Percentage:F2}%");
+Console.WriteLine($"{numsDividedBy3Mod0Percentage:F2}%");
+Console.WriteLine($"{numsDividedBy4Mod0Percentage:F2}%");
