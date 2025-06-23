bc52cd56e8044d20dc145d17397aa244a2375dfa
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-23 19:47:42 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Loops - Exercise/08. Account Balance/Program.cs
@@ -0,0 +1,20 @@
+﻿double balance = 0;
+string input;
+
+while ((input = Console.ReadLine()) != "End")
+{
+    double transactionAmount = double.Parse(input);
+
+    if (transactionAmount >= 0)
+    {
+        Console.WriteLine($"Increase: {transactionAmount:F2}");
+    }
+    else
+    {
+        Console.WriteLine($"Decrease: {-transactionAmount:F2}");
+    }
+
+    balance += transactionAmount;
+}
+
+Console.WriteLine($"Balance: {balance:F2}");
