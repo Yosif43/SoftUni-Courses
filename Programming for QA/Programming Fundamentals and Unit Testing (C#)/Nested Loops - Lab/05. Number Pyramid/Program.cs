50b6dd8b1836e6b226ed56cf27ef9a159396af30
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-24 20:10:29 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Nested Loops - Lab/05. Number Pyramid/Program.cs
@@ -0,0 +1,20 @@
+﻿int n = int.Parse(Console.ReadLine());
+int currentNumber = 1;
+
+for (int row = 1; currentNumber <= n; row++)
+{
+    for (int col = 1; col <= row; col++)
+    {
+        Console.Write(currentNumber + " ");
+        currentNumber++;
+        if (currentNumber > n)
+        {
+            break;
+        }
+    }
+    Console.WriteLine();
+    if (currentNumber > n)
+    {
+        break;
+    }
+}
