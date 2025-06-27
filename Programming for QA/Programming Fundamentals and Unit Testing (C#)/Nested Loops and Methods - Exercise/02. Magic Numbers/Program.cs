e880f311e0b51c9abbb7f0a6bfef54fad2c3bc5a
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-27 18:32:55 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Nested Loops and Methods - Exercise/02. Magic Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for  (int hundreds = 1; hundreds <= 9; hundreds++)
+{
+    for (int tens = 0; tens <= 9; tens++)
+    {
+        for (int singles = 0; singles <= 9; singles++)
+        {
+            int digitsProduct = hundreds * tens * singles;
+            if (digitsProduct == n)
+            {
+                Console.Write($"{hundreds}{tens}{singles} ");
+            }
+        }
+    }
+}
