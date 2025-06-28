056610d61111dc45e0ac908343a101493127c2fd
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-28 20:46:03 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Nested Loops and Methods - Exercise/04. Letter Combinations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class LetterCombinations
+{
+    static void Main()
+    {
+        // Read input letters
+        char startLetter = Console.ReadLine()[0];
+        char endLetter = Console.ReadLine()[0];
+        char excludedLetter = Console.ReadLine()[0];
+
+        int combinationsCount = 0;
+        string combinations = "";
+
+        // Generate combinations
+        for (char i = startLetter; i <= endLetter; i++)
+        {
+            for (char j = startLetter; j <= endLetter; j++)
+            {
+                for (char k = startLetter; k <= endLetter; k++)
+                {
+                    if (i != excludedLetter && j != excludedLetter && k != excludedLetter)
+                    {
+                        combinations += $"{i}{j}{k} ";
+                        combinationsCount++;
+                    }
+                }
+            }
+        }
+
+        // Print combinations and count
+        Console.WriteLine(combinations.Trim());
+        Console.WriteLine(combinationsCount);
+    }
+}
