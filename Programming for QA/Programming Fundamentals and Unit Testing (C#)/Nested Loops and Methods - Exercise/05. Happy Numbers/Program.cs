f598f29fb91c77d32db3ca6e0b5ea9fc9925bf83
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-28 20:46:34 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Nested Loops and Methods - Exercise/05. Happy Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿class HappyNumbers
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 1000; i <= 9999; i++)
+        {
+            int d1 = i / 1000;
+            int d2 = (i / 100) % 10;
+            int d3 = (i / 10) % 10;
+            int d4 = i % 10;
+
+            if (d1 + d2 == N && d3 + d4 == N)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
