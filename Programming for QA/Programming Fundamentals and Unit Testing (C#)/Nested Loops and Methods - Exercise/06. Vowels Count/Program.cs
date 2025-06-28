850422ea2964448a726cac58f51dcf060648cb87
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-06-28 20:46:59 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Nested Loops and Methods - Exercise/06. Vowels Count/Program.cs
@@ -0,0 +1,19 @@
+﻿string word = Console.ReadLine();
+
+int vowelsCount = CountVowelsInText(word);
+
+Console.WriteLine(vowelsCount);
+
+static int CountVowelsInText(string text)
+{
+    int vowelsCount = 0;
+    for (int i = 0; i < text.Length; i++)
+    {
+        char ch = char.ToLower(text[i]);
+        if (ch == 'a' || ch == 'e' || ch == 'o' || ch == 'u' || ch == 'i')
+        {
+            vowelsCount++;
+        }
+    }
+    return vowelsCount;
+}
