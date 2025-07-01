4464cc5ecc6056d55b0d992266259fc9b007891b
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-07-01 23:49:02 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Nested Loops and Methods - Exercise/07. Factorial Division/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class FactorialDivision
+{
+    static void Main(string[] args)
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+
+        double firstNumberFactorial = CalculateFactorial(firstNumber);
+        double secondNumberFactorial = CalculateFactorial(secondNumber);
+
+        double divisionResult = firstNumberFactorial / secondNumberFactorial;
+
+        Console.WriteLine($"{Math.Floor(divisionResult)}");
+    }
+
+    static double CalculateFactorial(int number)
+    {
+        if (number == 0)
+            return 1;
+
+        double factorial = 1;
+        for (int i = 1; i <= number; i++)
+        {
+            factorial *= i;
+        }
+
+        return factorial;
+    }
+}
