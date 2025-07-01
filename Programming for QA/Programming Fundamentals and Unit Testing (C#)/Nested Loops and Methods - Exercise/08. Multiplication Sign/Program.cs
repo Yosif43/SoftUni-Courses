fa6a2d09473c34a5e4dc989841a3ecb685fc37a9
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-07-01 23:49:28 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Nested Loops and Methods - Exercise/08. Multiplication Sign/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class MultiplicationSign
+{
+    static void Main()
+    {
+        int num1 = int.Parse(Console.ReadLine());
+        int num2 = int.Parse(Console.ReadLine());
+        int num3 = int.Parse(Console.ReadLine());
+
+        if (num1 == 0 || num2 == 0 || num3 == 0)
+        {
+            Console.WriteLine("zero");
+            return;
+        }
+
+        int negativeCount = 0;
+        if (num1 < 0) negativeCount++;
+        if (num2 < 0) negativeCount++;
+        if (num3 < 0) negativeCount++;
+
+        if (negativeCount % 2 == 0)
+        {
+            Console.WriteLine("positive");
+        }
+        else
+        {
+            Console.WriteLine("negative");
+        }
+    }
+}
