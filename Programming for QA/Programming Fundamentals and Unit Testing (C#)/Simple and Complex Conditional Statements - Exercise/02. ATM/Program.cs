66e4c7fe4e2ecfa2f347fe2d116a2c52ab1a1b81
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-07-02 18:54:12 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Simple and Complex Conditional Statements - Exercise/02. ATM/Program.cs
@@ -0,0 +1,7 @@
+﻿int balance = int.Parse(Console.ReadLine());
+int withdraw = int.Parse(Console.ReadLine());
+int limit  = int.Parse(Console.ReadLine());
+
+if (balance > withdraw && withdraw < limit) Console.WriteLine("The withdraw was successful.");
+else if (withdraw > limit) Console.WriteLine("The limit was exceeded.");
+else if (balance < withdraw && withdraw < limit) Console.WriteLine("Insufficient availability.");
