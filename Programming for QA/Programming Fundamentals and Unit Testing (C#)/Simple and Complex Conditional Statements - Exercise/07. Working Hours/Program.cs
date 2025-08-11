a1bd3f96520f6712652c7e9e1961f18aa2f4ea9f
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-08-11 10:11:01 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Simple and Complex Conditional Statements - Exercise/07. Working Hours/Program.cs
@@ -0,0 +1,9 @@
+﻿int hourofDay = int.Parse(Console.ReadLine());
+string dayofWeek = Console.ReadLine();
+
+if (dayofWeek == "Monday" || dayofWeek == "Tuesday" || dayofWeek == "Wednesday" || dayofWeek == "Thursday" || dayofWeek == "Friday" || dayofWeek == "Saturday")
+{
+    if (hourofDay >= 10 && hourofDay <= 18) Console.WriteLine("open");
+    else Console.WriteLine("closed");
+}
+else Console.WriteLine("closed");
