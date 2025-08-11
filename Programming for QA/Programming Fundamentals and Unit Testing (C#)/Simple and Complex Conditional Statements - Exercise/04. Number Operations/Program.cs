06ff291003d9166f6343d920910967f1d91b18e8
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-08-11 10:09:51 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Simple and Complex Conditional Statements - Exercise/04. Number Operations/Program.cs
@@ -0,0 +1,24 @@
+﻿double number1 = double.Parse(Console.ReadLine());
+double number2  = double.Parse(Console.ReadLine());
+string evOperator = Console.ReadLine();
+
+if (evOperator == "+")
+{
+    double result = number1 + number2;
+    Console.WriteLine($"{number1} {evOperator} {number2} = {result:F2}");
+}
+else if (evOperator == "-")
+{
+    double result = (number1 - number2);
+    Console.WriteLine($"{number1} {evOperator} {number2} = {result:F2}");
+}
+else if (evOperator == "*")
+{
+    double result = number1 * number2;
+    Console.WriteLine($"{number1} {evOperator} {number2} = {result:F2}");
+}
+else if (evOperator == "/")
+{
+    double result = number1 / number2;
+    Console.WriteLine($"{number1} {evOperator} {number2} = {result:F2}");
+}
