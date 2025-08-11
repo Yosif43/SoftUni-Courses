026c155da0ab2258527ddb8aaecdb27aef0a04aa
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-08-11 10:10:20 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Simple and Complex Conditional Statements - Exercise/05. Vacation Expenses/Program.cs
@@ -0,0 +1,50 @@
+﻿string season = Console.ReadLine();
+string accommodation = Console.ReadLine();
+int countofDays = int.Parse(Console.ReadLine());
+
+if  (accommodation == "Hotel")
+{
+    if (season == "Spring")
+    {
+        double result = (countofDays * 30) * 0.80;
+        Console.WriteLine($"{result:F2}");
+    }
+    else if (season == "Summer")
+    {
+        double result = countofDays * 50;
+        Console.WriteLine($"{result:F2}");
+    }
+    else if (season == "Autumn")
+    {
+        double result = (countofDays * 20) * 0.70;
+        Console.WriteLine($"{result:F2}");
+    }
+    else if (season == "Winter")
+    {
+        double result = (countofDays * 40) * 0.90;
+        Console.WriteLine($"{result:F2}");
+    }
+}
+else if (accommodation == "Camping")
+{
+    if (season == "Spring")
+    {
+        double result = (countofDays * 10) * 0.80;
+        Console.WriteLine($"{result:F2}");
+    }
+    else if (season == "Summer")
+    {
+        double result = countofDays * 30;
+        Console.WriteLine($"{result:F2}");
+    }
+    else if (season == "Autumn")
+    {
+        double result = (countofDays * 15) * 0.70;
+        Console.WriteLine($"{result:F2}");
+    }
+    else if (season == "Winter")
+    {
+        double result = (countofDays * 10) * 0.90;
+        Console.WriteLine($"{result:F2}");
+    }
+}
