d72e91778131747d558f4112506d8cdf01dfe3d7
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-08-11 10:10:39 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Simple and Complex Conditional Statements - Exercise/06. Product of Three Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿double num1 = double.Parse(Console.ReadLine());
+double num2 = double.Parse(Console.ReadLine());
+double num3 = double.Parse(Console.ReadLine());
+
+if (num1 == 0 || num2 == 0 || num3 == 0)
+{
+    Console.WriteLine("zero");
+}
+else
+{
+    int negativeCount = 0;
+    if (num1 < 0) negativeCount++;
+    if (num2 < 0) negativeCount++;
+    if (num3 < 0) negativeCount++;
+
+    Console.WriteLine(negativeCount % 2 == 0 ? "positive" : "negative");
+}
