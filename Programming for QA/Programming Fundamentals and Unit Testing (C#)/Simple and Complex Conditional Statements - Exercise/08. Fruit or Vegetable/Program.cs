9b861fbe172853daa9306dc6c6ad8205d3b94e79
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-08-11 10:11:18 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Simple and Complex Conditional Statements - Exercise/08. Fruit or Vegetable/Program.cs
@@ -0,0 +1,5 @@
+﻿string product = Console.ReadLine();
+
+if (product == "banana" || product == "apple" || product == "kiwi" || product == "cherry" || product == "lemon") Console.WriteLine("fruit");
+else if (product == "cucumber" || product == "pepper" || product == "carrot") Console.WriteLine("vegetable");
+else Console.WriteLine("unknown");
