1584845a1c74d403e2a5ba8cab804f7614e8fe25
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-08-11 10:11:56 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Simple and Complex Conditional Statements - Exercise/09. Sum Seconds/Program.cs
@@ -0,0 +1,18 @@
+﻿int athlete1 = int.Parse(Console.ReadLine());
+int athlete2 = int.Parse(Console.ReadLine());
+int athlete3 = int.Parse(Console.ReadLine());
+
+//int totalTime = athlete1 + athlete2 + athlete3;
+
+//int minutes = totalTime / 60;
+//int seconds = totalTime % 60;
+//string formattedSeconds = seconds.ToString().PadLeft(2, '0');
+//Console.WriteLine($"{minutes}:{formattedSeconds}");
+
+// or 
+
+int totalTime = athlete1 + athlete2 + athlete3;
+
+int minutes = totalTime / 60;
+int seconds = totalTime % 60;
+Console.WriteLine($"{minutes}:{seconds:D2}");
