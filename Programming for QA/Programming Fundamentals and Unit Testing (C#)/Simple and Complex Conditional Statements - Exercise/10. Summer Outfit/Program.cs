ddb88beac6afe411b9e49c31250f92b35de4c237
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-08-12 11:59:48 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Simple and Complex Conditional Statements - Exercise/10. Summer Outfit/Program.cs
@@ -0,0 +1,48 @@
+﻿int temperature = int.Parse(Console.ReadLine());
+string timeOfDay = Console.ReadLine();
+
+if (timeOfDay == "Morning")
+{
+    if (temperature >= 10 && temperature <= 18)
+    {
+        Console.WriteLine($"It's {temperature} degrees, get your Sweatshirt and Sneakers.");
+    }
+    else if (temperature > 18 &&  temperature <= 24)
+    {
+        Console.WriteLine($"It's {temperature} degrees, get your Shirt and Moccasins.");
+    }
+    else
+    {
+        Console.WriteLine($"It's {temperature} degrees, get your T-Shirt and Sandals.");
+    }
+}
+else if (timeOfDay == "Afternoon")
+{
+    if (temperature >= 10 && temperature <= 18)
+    {
+        Console.WriteLine($"It's {temperature} degrees, get your Shirt and Moccasins.");
+    }
+    else if (temperature > 18 && temperature <= 24)
+    {
+        Console.WriteLine($"It's {temperature} degrees, get your T-Shirt and Sandals.");
+    }
+    else
+    {
+        Console.WriteLine($"It's {temperature} degrees, get your Swim Suit and Barefoot.");
+    }
+}
+else if (timeOfDay == "Evening")
+{
+    if (temperature >= 10 && temperature <= 18)
+    {
+        Console.WriteLine($"It's {temperature} degrees, get your Shirt and Moccasins.");
+    }
+    else if (temperature > 18 && temperature <= 24)
+    {
+        Console.WriteLine($"It's {temperature} degrees, get your Shirt and Moccasins.");
+    }
+    else
+    {
+        Console.WriteLine($"It's {temperature} degrees, get your Shirt and Moccasins.");
+    }
+}
