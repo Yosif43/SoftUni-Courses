599e73713eef59123abc46017a737ed16a88e457
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-08-12 11:59:57 +0100

    Create program2.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Simple and Complex Conditional Statements - Exercise/10. Summer Outfit/program2.cs
@@ -0,0 +1,20 @@
+﻿int temperature = int.Parse(Console.ReadLine());
+string timeOfDay = Console.ReadLine();
+string outfit = "";
+string shoes = "";
+
+if (temperature >= 10 &&  temperature <= 18)
+{
+    if (timeOfDay == "Morning")
+    {
+        outfit = "Sweetshirt";
+        shoes = "Sneakers";
+    }
+    else if (timeOfDay == "Afternoon" || timeOfDay == "Evening")
+    {
+        outfit = "Shirt";
+        shoes = "Moccasins";
+    }
+}
+
+Console.WriteLine($"It's {temperature} degrees, get your {outfit} and {shoes}.");
