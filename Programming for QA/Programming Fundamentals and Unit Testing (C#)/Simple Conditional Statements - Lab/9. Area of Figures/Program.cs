c12ba12a104640530d7df2e91187be39043b7bbe
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-09-30 18:44:03 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Simple Conditional Statements - Lab/9. Area of Figures/Program.cs
@@ -0,0 +1,18 @@
+﻿string figure = Console.ReadLine();
+
+if (figure == "square") {
+    double num = double.Parse(Console.ReadLine());
+    double square = num * num;
+    Console.WriteLine($"{square:F2}");
+     }
+else if (figure == "rectangle") {
+    double num1 = double.Parse(Console.ReadLine());
+    double num2 = double.Parse(Console.ReadLine());
+    double rectangle = num1 * num2;
+    Console.WriteLine($"{rectangle:F2}");
+    }
+else if (figure == "circle") {
+    double number = double.Parse(Console.ReadLine());
+    double circle = Math.PI * number * number;
+    Console.WriteLine($"{circle:F2}");
+    }
