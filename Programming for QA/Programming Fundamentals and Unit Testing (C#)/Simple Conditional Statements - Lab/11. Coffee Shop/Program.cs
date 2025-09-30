283baa8b6b577de3f1d2a478a495f2b89e1b3395
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-09-30 18:44:32 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Simple Conditional Statements - Lab/11. Coffee Shop/Program.cs
@@ -0,0 +1,33 @@
+﻿string drink = Console.ReadLine();
+string extra = Console.ReadLine();
+
+double price = 0.0;
+const double CoffeePrice = 1.00;
+const double TeaPrice = 0.60;
+const double SugarPrice = 0.40;
+
+if (drink == "coffee")
+{
+    price += CoffeePrice;
+}
+else if (drink == "tea")
+{
+    price += TeaPrice;
+}
+else
+{
+    Console.WriteLine("Invalid drink name.");
+    return;
+}
+
+if (extra == "sugar")
+{
+    price += SugarPrice;
+}
+else if (extra != "no")
+{
+    Console.WriteLine("Invalid option for extra.");
+    return;
+}
+
+Console.WriteLine($"Final price: ${price:0.00}");
