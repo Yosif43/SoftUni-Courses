b1cce46e3b4f2a0cc4039ca0288f38caff106e4e
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-09-30 18:44:19 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Simple Conditional Statements - Lab/10. Valid Triangle/Program.cs
@@ -0,0 +1,6 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 < num2 + num3 && num2 < num1 + num3 && num3 < num1 + num2) Console.WriteLine("Valid Triangle");
+else Console.WriteLine("Invalid Triangle");
