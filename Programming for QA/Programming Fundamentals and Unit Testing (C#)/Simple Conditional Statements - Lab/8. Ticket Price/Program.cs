8e5c1b2502d8c489e62d1a9b1ce87a84a5ef8fad
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-09-30 18:43:50 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Simple Conditional Statements - Lab/8. Ticket Price/Program.cs
@@ -0,0 +1,4 @@
+﻿string ticket = Console.ReadLine();
+if (ticket == "student") Console.WriteLine("$1.00");
+else if (ticket == "regular") Console.WriteLine("$1.60");
+else Console.WriteLine("Invalid ticket type!");
