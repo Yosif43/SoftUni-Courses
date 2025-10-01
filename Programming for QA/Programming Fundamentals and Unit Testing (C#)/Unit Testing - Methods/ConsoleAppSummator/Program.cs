1f661c9fc6a2efc882f58e616a6b0dd9c9555aa3
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-01 19:36:12 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/Unit Testing - Methods/ConsoleAppSummator/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleAppSummator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static int Sum(int[] nums)
+        {
+            int result = 0;
+
+            foreach (int num in nums) {
+            result += num;
+            }
+            return result;
+        }
+    }
+}
