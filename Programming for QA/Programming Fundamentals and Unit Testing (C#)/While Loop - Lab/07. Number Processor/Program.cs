992d5e74d00d791b05db03b983a5d4b8dd471815
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-02 19:37:50 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/While Loop - Lab/07. Number Processor/Program.cs
@@ -0,0 +1,16 @@
+﻿int n = int.Parse(Console.ReadLine());
+string command = Console.ReadLine();
+
+while (command != "End")
+{
+    if (command == "Inc")
+    {
+        n++;
+    }
+    else if (command == "Dec")
+    {
+        n--;
+    }
+    command = Console.ReadLine();
+}
+Console.WriteLine(n);
