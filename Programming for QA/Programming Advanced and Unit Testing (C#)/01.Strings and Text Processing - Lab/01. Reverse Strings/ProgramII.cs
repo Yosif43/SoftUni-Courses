dbcf908bcff58cccc5e6b5bdc5f2e3371c5e662d
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-02 19:40:21 +0100

    Create ProgramII.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/01.Strings and Text Processing - Lab/01. Reverse Strings/ProgramII.cs
@@ -0,0 +1,10 @@
+﻿string input = Console.ReadLine();
+
+while (input != "end")
+{
+    char[] output = input.ToCharArray().Reverse().ToArray();
+    string reversed = string.Join("", output);
+    Console.WriteLine(input + " = " + reversed);
+
+    input = Console.ReadLine();
+}
