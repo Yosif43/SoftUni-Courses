085974216ccd9290a334078eb76d67503a3270bf
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-02 19:37:36 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Fundamentals and Unit Testing (C#)/While Loop - Lab/06. Number in Range/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+while (true)
+{
+    if (n >= 1 && n <= 100)
+    {
+        break;
+    }
+    n = int.Parse(Console.ReadLine());
+}
+Console.WriteLine(n);
