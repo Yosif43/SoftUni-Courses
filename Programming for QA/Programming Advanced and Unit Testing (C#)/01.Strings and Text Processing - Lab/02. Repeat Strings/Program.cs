7ed709e16ca8cc94beed681c9c358fdb915e2587
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-03 19:39:28 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/01.Strings and Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] words = Console.ReadLine().Split().ToArray();
+
+string output = "";
+
+foreach (string word in words)
+{
+    for (int i = 0; i < word.Length; i++)
+    {
+        output += word;
+    }
+}
+
+Console.WriteLine(output);
