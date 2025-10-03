d6ed75ba03cebeae66a77f017fc50f44799609ab
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-03 19:40:02 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/01.Strings and Text Processing - Lab/04. Text Filter/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] words = Console.ReadLine().Split(", ").ToArray();
+string text = Console.ReadLine();
+
+foreach (string word in words)
+{
+    if (text.Contains(word))
+    {
+        string replacement = new string('*', word.Length);
+        text = text.Replace(word, replacement);
+    }
+}
+Console.WriteLine(text);
