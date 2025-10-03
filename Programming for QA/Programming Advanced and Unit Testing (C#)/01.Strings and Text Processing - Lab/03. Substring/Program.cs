396845839cd9cc2f84e6eb85d0dbd34108e9a82e
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-03 19:39:44 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/01.Strings and Text Processing - Lab/03. Substring/Program.cs
@@ -0,0 +1,13 @@
+﻿string word = Console.ReadLine();
+string text = Console.ReadLine();
+
+while (text.Contains(word))
+{
+    int wordIndex = text.IndexOf(word);
+    string before = text.Substring(0, wordIndex);
+    string after = text.Substring(wordIndex + word.Length);
+
+    text = before + after;
+}
+
+Console.WriteLine(text);
