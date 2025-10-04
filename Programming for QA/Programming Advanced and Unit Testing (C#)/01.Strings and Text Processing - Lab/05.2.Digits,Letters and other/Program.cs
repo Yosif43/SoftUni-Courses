fa8ebbfff6790e0298efa05eef9f4eac03d9196f
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-04 17:52:01 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/01.Strings and Text Processing - Lab/05.2.Digits,Letters and other/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+string input = Console.ReadLine();
+
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+
+StringBuilder digits = new StringBuilder();
+StringBuilder letters = new StringBuilder();
+StringBuilder others = new StringBuilder();
+
+foreach (char ch in input)
+{
+    if (char.IsDigit(ch))
+    {
+        digits.Append(ch);
+    }
+    else if (char.IsLetter(ch))
+    {
+        letters.Append(ch);
+    }
+    else { others.Append(ch); }
+}
+stopwatch.Stop();
+Console.WriteLine(digits);
+Console.WriteLine(letters);
+Console.WriteLine(others);
