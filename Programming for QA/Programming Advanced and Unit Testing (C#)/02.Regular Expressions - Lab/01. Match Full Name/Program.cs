c1ff26b7085ce085821898600f1ba1d616aab036
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-04 17:52:28 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/02.Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+string text = Console.ReadLine();
+
+string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+Regex regex = new Regex(pattern);
+MatchCollection matches = regex.Matches(text);
+List<string> result = new List<string>();
+
+foreach (Match match in matches)
+{
+    result.Add(match.Value);
+}
+
+Console.WriteLine(string.Join(" ", result));
