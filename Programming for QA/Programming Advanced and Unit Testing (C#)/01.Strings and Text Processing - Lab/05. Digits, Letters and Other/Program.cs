97978efa7a6253b577b50b481a78d0ecb04c9623
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-04 17:51:51 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/01.Strings and Text Processing - Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,18 @@
+﻿string input = Console.ReadLine();
+
+string digits = "";
+string letters = "";
+string others = "";
+
+foreach (char ch in input)
+{
+    if (char.IsDigit(ch)){
+        digits += ch;
+    }else if (char.IsLetter(ch))
+    {
+        letters += ch;
+    }else { others += ch; }
+}
+Console.WriteLine(digits);
+Console.WriteLine(letters);
+Console.WriteLine(others);
