45f92f4c7addef9f84809c048d16cfcef24d9d1c
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-05 16:51:56 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/02.Regular Expressions - Lab/03. Match Dates/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+string text = Console.ReadLine();
+
+string pattern = @"(?'day'\d{2})(?'del'[\/\-\.])(?'month'[A-Z][a-z]{2})\'del'(?'year'\d{4})";
+
+Regex regex = new Regex(pattern);
+MatchCollection matches = regex.Matches(text);
+
+foreach (Match match in matches)
+{
+    string day = match.Groups["day"].Value;
+    string month = match.Groups["month"].Value;
+    string year = match.Groups["year"].Value;
+    Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+}
