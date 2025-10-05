0f56e2d80269cd50a8e8d34e90e41503c67ce6a3
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-05 16:51:11 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/02.Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+string text = Console.ReadLine();
+
+string pattern = @"\+359([\ \-])2\1\d{3}\1\d{4}\b";
+
+Regex regex = new Regex(pattern);
+
+MatchCollection matchCollection = regex.Matches(text);
+
+List<string> result = new List<string>();
+
+foreach (Match match in matchCollection)
+{
+    result.Add(match.Value);
+}
+
+Console.WriteLine(string.Join(", ", result));
