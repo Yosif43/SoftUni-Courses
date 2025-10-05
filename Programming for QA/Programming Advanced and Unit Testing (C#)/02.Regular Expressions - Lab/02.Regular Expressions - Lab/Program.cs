456639b667c5f38794c2700db4d3227ff2b33796
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-05 16:51:42 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/02.Regular Expressions - Lab/02.Regular Expressions - Lab/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+using System.Text.RegularExpressions.MatchCollection;
+
+string text = "Today is 2015-05-11, but tomorrow will be 2015-05-12";
+
+string pattern = @"\d{4}-\d{2}-\d{2}";
+
+Regex regex = new Regex(pattern);
+
+bool containsValidDate = regex.IsMatch(text);
+Match match = regex.Match(text);
+MatchCollection matches = regex.Matches(text);
+
+Console.WriteLine(containsValidDate);
+Console.WriteLine(match);
+Console.WriteLine(matches);
