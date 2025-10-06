2b0bb2e7ceaaea5fd7b51fab1f3eeae62b02f65d
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-06 20:32:50 +0100

    Create EmailValidatorTests.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/03.Strings and Regular Expressions - Unit Testing Exercise/TestApp.UnitTests/EmailValidatorTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class EmailValidatorTests
+{
+    [TestCase("stoyanshopov032@gmail.com")]
+    [TestCase("stoyan.shopov@gmail.com")]
+    [TestCase("stoyan_shopov@gmail.com")]
+    [TestCase("stoyan%shopov@gmail.com")]
+    [TestCase("stoyan+shopov@gmail.com")]
+    [TestCase("stoyan-shopov@gmail.com")]
+    [TestCase("stoyan-shopov@gmail123.com")]
+    [TestCase("stoyan-shopov@gmail-123.com")]
+    [TestCase("stoyan-shopov@gmail.123.com")]
+    public void Test_ValidEmails_ReturnsTrue(string email)
+    {
+        // Arrange
+
+        // Act
+        bool result = EmailValidator.IsValidEmail(email);
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
+
+    [TestCase("stoyanshopov032@gmailcom")]
+    [TestCase("stoyan.shopovgmail.com")]
+    [TestCase("stoyan_shopov@@gmail.com")]
+    [TestCase("@gmail.com")]
+    [TestCase("gmail.com")]
+    [TestCase("stoyan-shopovgmailcom")]
+    [TestCase("")]
+    [TestCase("1")]
+
+    public void Test_InvalidEmails_ReturnsFalse(string email)
+    {
+        // Arrange
+
+        // Act
+        bool result = EmailValidator.IsValidEmail(email);
+        
+        // Assert
+        Assert.That(result, Is.False);
+    }
+}
