f2b9e8098c0ec0ee37dd28bfbeb8bf0ca78a5222
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-08 22:04:15 +0100

    Create MatchPhoneNumbersTests.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/03.Strings and Regular Expressions - Unit Testing Exercise/TestApp.UnitTests/MatchPhoneNumbersTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class MatchPhoneNumbersTests
+{
+    
+    [TestCase("+359-2-124-5678, +359 2 986 5432, +359-2-555-5555", "+359-2-124-5678, +359 2 986 5432, +359-2-555-5555")]
+    [TestCase("359-2-124-5678, +159 2 986 5432, +351-2-555-5555, +359 2-124-5678, +359-1-124-5678," +
+        "+359-2-24-5678, +359-2-124-56781", "")]
+    [TestCase("", "")]
+    [TestCase("+359-2-666-1234, +359 21 666 1234, +359 2 444 1236", "+359-2-666-1234, +359 2 444 1236")]
+    [TestCase("+359-2-666-1234, +359 21 666 1234, +359 2 444 1236, +351-2-555-5555, +359 2-124-5678", "+359-2-666-1234, +359 2 444 1236")]
+
+    public void Test_Match_ValidPhoneNumbers_ReturnsMatchedNumbers(string input, string expectedResult)
+    {
+        // Act
+        string result = MatchPhoneNumbers.Match(input);
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
