ecb0c5d4b32128381b4288c9e6814077ad606090
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-09 21:08:48 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/06.Dictionaries, Lambda and LINQ/01. Count Real Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+List<double> numbers = Console.ReadLine()
+    .Split()
+    .Select(double.Parse)
+    .ToList();
+
+SortedDictionary<double, int> counter = new SortedDictionary<double, int>();
+
+foreach (var number in numbers)
+{
+    if (counter.ContainsKey(number))
+    {
+        counter[number]++;
+    }
+    else
+    {
+        counter.Add(number, 1);
+    }
+}
+foreach (KeyValuePair<double, int> elements in counter)
+{
+    Console.WriteLine($"{elements.Key} -> {elements.Value}");
+}
+
+
