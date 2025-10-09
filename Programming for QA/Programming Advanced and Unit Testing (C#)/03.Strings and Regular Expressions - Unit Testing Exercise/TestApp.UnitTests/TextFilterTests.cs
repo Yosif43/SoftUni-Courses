8c253b8896e8e881a7798cce111f9111fd1ffc52
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-09 21:08:10 +0100

    Create TextFilterTests.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/03.Strings and Regular Expressions - Unit Testing Exercise/TestApp.UnitTests/TextFilterTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class TextFilterTests
+{
+    // TODO: finish the test
+    [Test]
+    public void Test_Filter_WhenNoBannedWords_ShouldReturnOriginalText()
+    {
+        // Arrange
+        string[] bannedWords = new string[] { };
+        string text = "Lorem ipsum etc.";
+        string expectedResult = "Lorem ipsum etc.";
+
+        // Act
+        string actualResult = TextFilter.Filter(bannedWords, text);
+        // Assert
+        Assert.That(actualResult, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void Test_Filter_WhenBannedWordExists_ShouldReplaceBannedWordWithAsterisks()
+    {
+        // Arrange
+        string[] bannedWords = new string[] { "Lorem" };
+        string text = "Lorem ipsum etc.";
+        string expectedResult = "***** ipsum etc.";
+        // Act
+        string actualResult = TextFilter.Filter(bannedWords, text);
+        // Assert
+        Assert.That(actualResult, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void Test_Filter_WhenBannedWordsAreEmpty_ShouldReturnOriginalText()
+    {
+        // Arrange
+        string[] bannedWords = new string[] { };
+        string text = "Lorem ipsum etc.";
+        string expectedResult = "Lorem ipsum etc.";
+        // Act
+        string actualResult = TextFilter.Filter(bannedWords, text);
+        // Assert
+        Assert.That(actualResult, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void Test_Filter_WhenBannedWordsContainWhitespace_ShouldReplaceBannedWord()
+    {
+        // Arrange
+        string[] bannedWords = new string[] { "Lorem ipsum" };
+        string text = "Lorem ipsum etc.";
+        string expectedResult = "*********** etc.";
+        // Act
+        string actualResult = TextFilter.Filter(bannedWords, text);
+        // Assert
+        Assert.That(actualResult, Is.EqualTo(expectedResult));
+    }
+}
