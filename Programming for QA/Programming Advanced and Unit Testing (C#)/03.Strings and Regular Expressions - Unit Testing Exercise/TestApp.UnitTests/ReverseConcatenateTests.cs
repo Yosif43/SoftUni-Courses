f8a453bc9476ade8332e95d5bd6b83477e1d5b50
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-09 21:07:54 +0100

    Create ReverseConcatenateTests.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/03.Strings and Regular Expressions - Unit Testing Exercise/TestApp.UnitTests/ReverseConcatenateTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Linq;
+
+namespace TestApp.UnitTests;
+
+public class ReverseConcatenateTests
+{
+
+    [TestCase(new string[] { }, "")]
+    [TestCase(new string[] { "" }, "")]
+    [TestCase(new string[] { "yosko" }, "yosko")]
+    [TestCase(new string[] { "Razlog", "Sofia", "Varna" }, "VarnaSofiaRazlog")]
+    [TestCase(new string[] { null }, "")]
+    [TestCase(null, "")]
+    [TestCase(new string[] { "" }, "")]
+
+    [TestCase(new string[] { "Ivan Shopov", "Ilko Kirilov", "Some More Words", "To be Added" }, "To be AddedSome More WordsIlko KirilovIvan Shopov")]
+    public void Test_ReverseAndConcatenateStrings_EmptyInput_ReturnsEmptyString(
+        string[] input,
+        string expectedResult)
+    {
+        // Act
+        string result = ReverseConcatenate.ReverseAndConcatenateStrings(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
