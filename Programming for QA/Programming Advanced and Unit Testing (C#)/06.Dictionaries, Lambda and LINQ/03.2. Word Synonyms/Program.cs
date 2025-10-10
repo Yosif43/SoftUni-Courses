9f6f9919a9acd0589787c2474c88552af4a8665a
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-10 21:35:24 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/06.Dictionaries, Lambda and LINQ/03.2. Word Synonyms/Program.cs
@@ -0,0 +1,24 @@
+﻿var count = int.Parse(Console.ReadLine());
+
+var wordsWithSynonyms = new Dictionary<string, List<string>>();
+
+for (var i = 0; i < count; i++)
+{
+    var word = Console.ReadLine();
+    var synonym = Console.ReadLine();
+
+    if (wordsWithSynonyms.ContainsKey(word))
+    {
+        wordsWithSynonyms[word].Add(synonym);
+    }
+    else
+    {
+        wordsWithSynonyms.Add(word, new() { synonym });
+    }
+}
+
+foreach (var word in wordsWithSynonyms)
+{
+    string synonym = string.Join(", ", word.Value);
+    Console.WriteLine($"{word.Key} - {synonym}");
+}
