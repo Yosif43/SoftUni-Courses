e30b0de6ff053c5794fb649abc969abda1246dd6
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-10 21:34:48 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/06.Dictionaries, Lambda and LINQ/02. Odd Occurrences/Program.cs
@@ -0,0 +1,24 @@
+﻿List<string> words = Console.ReadLine().ToLower().Split().ToList();
+
+Dictionary<string, int> wordCounter = new Dictionary<string, int>();
+
+
+foreach (string word in words)
+{
+    if (wordCounter.ContainsKey(word))
+    {
+        wordCounter[word]++;
+    }
+    else
+    {
+        wordCounter.Add(word, 1);
+    }
+}
+
+foreach (var pair in wordCounter)
+{
+    if (pair.Value % 2 != 0)
+    {
+        Console.Write(pair.Key + " ");
+    }
+}
