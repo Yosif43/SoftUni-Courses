cb04dd1c628ead1bb4c6afa46786de17c7c5d9c5
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-11 19:03:50 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/06.Dictionaries, Lambda and LINQ/04. Word Filter/Program.cs
@@ -0,0 +1,16 @@
+﻿var words = Console.ReadLine().Split().ToList();
+
+List<string> filtered = new List<string>();
+
+foreach(string word in words)
+{
+    if (word.Length % 2 == 0)
+    {
+        filtered.Add(word);
+    }
+}
+
+foreach(string word in filtered)
+{
+    Console.WriteLine(word);
+}
