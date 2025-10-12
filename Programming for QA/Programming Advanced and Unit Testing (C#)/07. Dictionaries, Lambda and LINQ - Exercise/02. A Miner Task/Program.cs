012ca6e8fdc8db3b492a8233fdfa724002fe599b
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-12 19:33:27 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/07. Dictionaries, Lambda and LINQ - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,24 @@
+﻿Dictionary<string, int> resources = new Dictionary<string, int>();
+
+var resource = Console.ReadLine();
+
+while ( resource != "stop")
+{
+     var quantity = int.Parse(Console.ReadLine());
+
+    if (resources.ContainsKey(resource))
+    {
+        resources[resource] += quantity;
+    }
+    else
+    {
+        resources.Add(resource, quantity);
+    }
+
+     resource = Console.ReadLine();
+}
+
+foreach( var kvp in resources)
+{
+    Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+}
