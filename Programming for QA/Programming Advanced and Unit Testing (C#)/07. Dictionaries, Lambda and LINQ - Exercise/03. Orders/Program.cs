d29044204dbf90d7093fad5bc5aed406ca88a177
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-12 19:34:05 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/07. Dictionaries, Lambda and LINQ - Exercise/03. Orders/Program.cs
@@ -0,0 +1,35 @@
+﻿var input = Console.ReadLine();
+
+var products = new Dictionary<string, List<double>>();
+
+while(input != "buy")
+{
+    string[] inputArray = input.Split();
+    string product = inputArray[0];
+    double price = double.Parse(inputArray[1]);
+    double quantity = double.Parse(inputArray[2]);
+
+    if (!products.ContainsKey(product))
+    {
+        products.Add(product, new List<double>());
+        products[product].Add(price);
+        products[product].Add(quantity);
+    }
+    else
+    {
+        products[product][0] = price;
+        products[product][1] += quantity;
+    }
+
+    input = Console.ReadLine();
+}
+
+foreach(var pair in products)
+{
+    string product = pair.Key;
+    double price = pair.Value[0];
+    double quantity = pair.Value[1];
+    double amount = price * quantity;
+
+    Console.WriteLine($"{product} -> {amount:F2}");
+}
