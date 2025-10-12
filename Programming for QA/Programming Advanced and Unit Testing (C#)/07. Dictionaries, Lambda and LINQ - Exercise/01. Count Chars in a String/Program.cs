fd0cf49f6d68fbcaff3a1dc02e865ea4b497ccf6
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-12 19:32:52 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/07. Dictionaries, Lambda and LINQ - Exercise/01. Count Chars in a String/Program.cs
@@ -0,0 +1,23 @@
+﻿var word = Console.ReadLine();
+
+Dictionary<char, int> characters = new Dictionary<char, int>();
+
+foreach (char ch in word)
+{
+    if (ch != ' ')
+    {
+        if (characters.ContainsKey(ch))
+        {
+            characters[ch]++;
+        }
+        else
+        {
+            characters.Add(ch, 1);
+        }
+    }
+}
+
+foreach(var pair in characters)
+{
+    Console.WriteLine($"{pair.Key} -> {pair.Value}");
+}
