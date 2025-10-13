54481c46d4935b5137d30ea0f93e531e709fb3da
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-13 17:31:59 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/07. Dictionaries, Lambda and LINQ - Exercise/06. Company Users/Program.cs
@@ -0,0 +1,34 @@
+﻿var companiesWithEmployees = new Dictionary<string, List<string>>();
+
+string input = Console.ReadLine();
+
+while (input != "End")
+{
+    string[] inputParams = input.Split(" -> ");
+    string company = inputParams[0];
+    string employeeID = inputParams[1];
+
+    if (companiesWithEmployees.ContainsKey(company))
+    {
+        if (!companiesWithEmployees[company].Contains(employeeID))
+        {
+            companiesWithEmployees[company].Add(employeeID);
+        }
+    }
+    else
+    {
+        companiesWithEmployees.Add(company, new List<string>() { employeeID });
+    }
+
+
+    input = Console.ReadLine();
+}
+
+foreach(var pair in companiesWithEmployees)
+{
+    Console.WriteLine(pair.Key);
+    foreach(var employee in pair.Value)
+    {
+        Console.WriteLine($"-- {employee}");
+    }
+}
