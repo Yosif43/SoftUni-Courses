02169494cfac73122e65ade7dd5ce93b6127c295
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-13 17:31:40 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/07. Dictionaries, Lambda and LINQ - Exercise/05. Student Academy/Program.cs
@@ -0,0 +1,29 @@
+﻿var studentsWithGrades = new Dictionary<string, List<double>>();
+
+int count = int.Parse(Console.ReadLine());
+
+for  (int i = 0; i < count; i++)
+{
+    string student = Console.ReadLine();
+    double grade = double.Parse(Console.ReadLine());
+
+    if (studentsWithGrades.ContainsKey(student))
+    {
+        studentsWithGrades[student].Add(grade);
+    }
+    else
+    {
+        studentsWithGrades.Add(student, new List<double> {grade});
+    }
+}
+
+foreach(var pair in studentsWithGrades)
+{
+    string student = pair.Key;
+    double averageGrade = pair.Value.Average();
+    if (averageGrade >= 4.50)
+    {
+        Console.WriteLine($"{student} -> {averageGrade:F2}");
+    }
+    
+}
