6dc2da30d93e38b635d31a8bcd28bd4baaee3f7e
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-10-13 17:31:16 +0100

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/07. Dictionaries, Lambda and LINQ - Exercise/04. SoftUni Parking/Program.cs
@@ -0,0 +1,44 @@
+﻿var numberOfCommands = int.Parse(Console.ReadLine());
+
+var usersWirhPlates = new Dictionary<string, string>();
+
+for  (int i = 0; i < numberOfCommands; i++)
+{
+    string[] commandParams = Console.ReadLine().Split();
+    string command = commandParams[0];
+    string user = commandParams[1];
+    
+
+    if (command == "register")
+    {
+        string plate = commandParams[2];
+
+        if (!usersWirhPlates.ContainsKey(user))
+        {
+            usersWirhPlates.Add(user, plate);
+            Console.WriteLine($"{user} registered {plate} successfully");
+        }
+        else
+        {
+            Console.WriteLine($"ERROR: already registered with plate number {usersWirhPlates[user]}");
+        }
+    }
+    else if (command == "unregister")
+    {
+        if (usersWirhPlates.ContainsKey(user))
+        {
+            usersWirhPlates.Remove(user);
+            Console.WriteLine($"{user} unregistered successfully");
+        }
+        else
+        {
+            Console.WriteLine($"ERROR: user {user} not found");
+        }
+    }
+
+}
+
+foreach (var pair in usersWirhPlates)
+{
+    Console.WriteLine($"{pair.Key} => {pair.Value}");
+}
