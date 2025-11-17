9b9d490fc9a7b7000dac1ef5a639126c88835654
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-11-17 18:07:06 +0000

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/09.Objects and Classes - Lab/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _01._Songs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Song> songList = new List<Song>();
+
+            int numberOfSongs = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < numberOfSongs; i++)
+            {
+                string[] inputParams = Console.ReadLine().Split("_");
+                string songListType = inputParams[0];
+                string songName = inputParams[1];
+                string songTime = inputParams[2];
+
+                Song currentSong = new Song();
+                currentSong.TypeList = songListType;
+                currentSong.Name = songName;
+                currentSong.Time = songTime;
+
+                songList.Add(currentSong);
+
+            }
+
+            string currentTypeList = Console.ReadLine();
+
+            if (currentTypeList == "all") 
+            {
+                foreach (Song song in songList)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                List<Song> filteredSongs = songList
+                                            .Where(s => s.TypeList == currentTypeList)
+                                            .ToList();
+                foreach (Song song in filteredSongs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+        }
+    }
+
+    public class Song 
+    { 
+        public string TypeList { get; set; }
+        public string Name { get; set; }
+        public string Time { get; set; }
+
+
+    }
+}
