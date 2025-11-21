2e5afd29b4e3ac46f5f278e94cb280a06d869af0
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-11-21 22:52:18 +0000

    Create Car.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/10. Defining Classes - Lab/CarManufacturer1/CarManufacturer/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+
+        public string Make { get => make; set => make = value; }
+        public string Model { get => model; set => model = value; }
+        public int Year { get => year; set => year = value; }
+    }
+}
+
