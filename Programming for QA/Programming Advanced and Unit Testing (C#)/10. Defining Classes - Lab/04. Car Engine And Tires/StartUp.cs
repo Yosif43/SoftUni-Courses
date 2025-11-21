ff13d29fab01d843385a23e331520b8ad1732888
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-11-21 22:51:46 +0000

    Create StartUp.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/10. Defining Classes - Lab/04. Car Engine And Tires/StartUp.cs
@@ -0,0 +1,10 @@
+﻿namespace _04._Car_Engine_And_Tires
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
