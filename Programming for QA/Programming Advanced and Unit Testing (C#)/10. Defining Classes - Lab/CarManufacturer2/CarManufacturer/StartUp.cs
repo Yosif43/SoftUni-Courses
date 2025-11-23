78350b1f5134ec82397fae964bdb9bff789bec14
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-11-23 21:47:28 +0000

    Create StartUp.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/10. Defining Classes - Lab/CarManufacturer2/CarManufacturer/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+            {
+                car.Make = "VW";
+                car.Model = "MK3";
+                car.Year = 1992;
+                car.FuelQuantity = 200;
+                car.FuelConsumption = 8;
+                car.Drive(20);
+                Console.WriteLine(car.WhoAmI());
+            }
+        }
+    }
+}
