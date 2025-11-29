3c6aa82875994bf44fda27d87a40cf8c5d6c257b
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-11-29 19:32:33 +0000

    Create PlanetTests.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/12. Unit Testing Exercise Objects and Classes/Unit-Testing-Exercise/TestApp.UnitTests/PlanetTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class PlanetTests
+{
+    [Test]
+    public void Test_CalculateGravity_ReturnsCorrectCalculation()
+    {
+        // Arrange
+        Planet earth = new("Earth", 12742, 149600000, 1);
+        double mass = 1000;
+        double expectedGravity = mass * 6.67430e-11 / Math.Pow(earth.Diameter / 2, 2);
+
+        // Act
+        double actualGravity = earth.CalculateGravity(mass);
+        // Assert
+        Assert.That(actualGravity, Is.EqualTo(expectedGravity));
+    }
+
+    [Test]
+    public void Test_GetPlanetInfo_ReturnsCorrectInfo()
+    {
+        // Arrange
+        Planet earth = new("Earth", 12742, 149600000, 1);
+        string expectedInfo = $"Planet: Earth{Environment.NewLine}" +
+            $"Diameter: 12742 km{Environment.NewLine}" +
+            $"Distance from the Sun: 149600000 km{Environment.NewLine}" +
+            $"Number of Moons: 1";
+
+        // Act
+        string actualInfo = earth.GetPlanetInfo();
+        // Assert
+        Assert.That(actualInfo, Is.EqualTo(expectedInfo));
+    }
+}
