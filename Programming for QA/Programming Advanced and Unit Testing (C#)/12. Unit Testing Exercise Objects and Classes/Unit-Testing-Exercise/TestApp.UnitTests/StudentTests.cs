4cd21021621719f1a1e51e62cdc917b970a384d0
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-11-30 09:31:13 +0000

    Create StudentTests.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/12. Unit Testing Exercise Objects and Classes/Unit-Testing-Exercise/TestApp.UnitTests/StudentTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class StudentTests
+{
+    private Student _student;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this._student = new();
+    }
+
+    // TODO: finish test
+    [Test]
+    public void Test_AddAndGetByCity_ReturnsStudentsInCity_WhenCityExists()
+    {
+        // Arrange
+        string[] students = { "John Doe 25 Sofia", "Jane Smith 22 Varna", "Alice Johnson 20 Sofia" };
+        string expected = $"John Doe is 25 years old.{Environment.NewLine}Alice Johnson is 20 years old.";
+
+        // Act
+        string result = _student.AddAndGetByCity(students, "Sofia");
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_AddAndGetByCity_ReturnsEmptyString_WhenCityDoesNotExist()
+    {
+        // Arrange
+        string[] students = { "John Doe 25 Sofia", "Jane Smith 22 Varna", "Alice Johnson 20 Sofia" };
+        string expected = $"John Doe is 25 years old.{Environment.NewLine}Alice Johnson is 20 years old.";
+        // Act
+        string result = _student.AddAndGetByCity(students, "Plovdiv");
+        // Assert
+        Assert.That(result, Is.EqualTo(string.Empty));
+    }
+
+    [Test]
+    public void Test_AddAndGetByCity_ReturnsEmptyString_WhenNoStudentsGiven()
+    {
+        // Arrange
+        string[] students = Array.Empty<string>();
+        
+        // Act
+        string result = _student.AddAndGetByCity(students, "Plovdiv");
+        // Assert
+        Assert.That(result, Is.EqualTo(string.Empty));
+    }
+}
