15485e22d08ee369b1023595e14dfa28bffce62d
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-11-30 09:31:53 +0000

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/13. Exceptions and Error Handling - Lab/01. Square Root/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _01._Square_Root
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            try
+            {
+                double number = ParsePositiveNumber(input);
+                double result = Math.Sqrt(number);
+                Console.WriteLine(result);
+             
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+
+        }
+
+        static double ParsePositiveNumber(string input)
+        {
+
+            bool isParsed = double.TryParse(input, out double num);
+
+            if (!isParsed)
+            {
+                throw new FormatException("Invalid input.");
+            }
+
+            if (num < 0)
+            {
+                throw new ArgumentException("Invalid number.");
+            }
+
+
+            return num;
+        }
+    }
+}
