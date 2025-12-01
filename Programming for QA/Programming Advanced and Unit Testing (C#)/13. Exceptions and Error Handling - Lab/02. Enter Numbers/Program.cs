1af0bd639d2888ccb99168ab2b7640eeca6d17c8
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-12-01 17:31:32 +0000

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/13. Exceptions and Error Handling - Lab/02. Enter Numbers/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _02._Enter_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            int start = 1;
+            int end = 100;
+
+            while (numbers.Count < 10)
+            {
+                try
+                {
+                    int number = ReadNumber(start, end);
+                    numbers.Add(number);
+                    start = number;
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+             
+            }
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+        static int ReadNumber(int start, int end)
+        {
+            string input = Console.ReadLine();
+            bool isParsed = int.TryParse(input, out int output);
+            if (!isParsed)
+            {
+                throw new FormatException("Invalid Number!");
+            }
+            if (output <= start || output >= end)
+            {
+                throw new ArgumentException($"Your number is not in range {start} - {end}!");
+            }
+            return output;
+        }
+    }
+}
