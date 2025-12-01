72947cc7ce18a6080b5e26601cef826b8356e50f
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-12-01 17:31:47 +0000

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/13. Exceptions and Error Handling - Lab/03. Sum of Integers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _03._Sum_of_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split();
+            int sum = 0;
+
+            foreach (string input in inputs)
+            {
+                try
+                {
+                    int number = int.Parse(input);
+                    sum += number;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{input}' is in wrong format!");
+                }
+                catch(OverflowException)
+                {
+                    Console.WriteLine($"The element '{input}' is out of range!");
+                }
+                finally { Console.WriteLine($"Element '{input}' processed - current sum: {sum}"); }
+            }
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+        }
+    }
+}
