665d7a67045c796890706480e7fc82c8cef3491a
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-12-03 17:30:51 +0000

    Create Person.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/15. Encapsulation and Inheritance - Lab/OOP-Principles-Encapsulation-Inheritance/PersonInfo/Person.cs
@@ -0,0 +1,51 @@
+﻿ using System;
+
+namespace PersonInfo;
+
+public class Person
+{
+    private string firstName;
+    private string lastName;
+    private int age;
+
+    public Person(string firstName, string lastName, int age)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+    }
+
+    public string FirstName { get => firstName;
+        private set
+        {
+            if (value.Length < 3) 
+            {
+                throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+            }
+
+            firstName = value;
+        }
+    }
+    public string LastName { get => lastName; private set 
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+            }
+            lastName = value; 
+        } 
+    }
+    public int Age { get => age; private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Age cannot be zero or a negative integer!");
+            }
+            age = value;
+        } 
+    }
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName} is {Age} years old.";
+    }
+}
