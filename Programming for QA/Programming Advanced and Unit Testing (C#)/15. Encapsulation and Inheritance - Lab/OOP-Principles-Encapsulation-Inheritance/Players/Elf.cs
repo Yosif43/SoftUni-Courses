2afc717af47f5dd8ab76761e2057fa3da56d005b
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-12-04 21:59:36 +0000

    Create Elf.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/15. Encapsulation and Inheritance - Lab/OOP-Principles-Encapsulation-Inheritance/Players/Elf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Players
+{
+    public class Elf : Hero
+    {
+        public Elf(string username, int level) : base(username, level) 
+        { 
+        }
+        
+    }
+}
