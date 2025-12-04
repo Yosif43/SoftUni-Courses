1469347497ee6e46d2a222d6fe9d140784cc0780
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-12-04 21:59:45 +0000

    Create Hero.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/15. Encapsulation and Inheritance - Lab/OOP-Principles-Encapsulation-Inheritance/Players/Hero.cs
@@ -0,0 +1,20 @@
+﻿namespace Players;
+
+public class Hero
+{
+    public string Username;
+    public int Level;
+
+    public Hero(string username, int level)
+    {
+        Username = username;
+        Level = level;
+    }
+
+    public override string ToString()
+    {
+        string output = $"Type: {GetType().Name} Username: {Username} Level: {Level}";
+
+        return output;
+    }
+}
