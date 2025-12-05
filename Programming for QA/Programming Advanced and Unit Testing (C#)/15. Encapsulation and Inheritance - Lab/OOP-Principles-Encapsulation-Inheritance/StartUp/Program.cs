e3fad3b6f7b889e0e919143962c4ca7c964f5a98
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-12-05 17:22:55 +0000

    Create Program.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/15. Encapsulation and Inheritance - Lab/OOP-Principles-Encapsulation-Inheritance/StartUp/Program.cs
@@ -0,0 +1,31 @@
+﻿using Players;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartUp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            Hero hero = new Hero("Yosko", 100);
+            Elf elf = new Elf("Relinda", 80);
+            MuseElf museElf = new MuseElf("Jastralina", 110);
+            Knight knight = new Knight("Frozenfinger", 80);
+            DarkKnight darkKnight = new DarkKnight("Lastpain", 80);
+            BladeKnight bladeKnight = new BladeKnight("Finalhit", 80);
+
+            Console.WriteLine(hero);
+            Console.WriteLine(elf);
+            Console.WriteLine(museElf);
+            Console.WriteLine(knight);
+            Console.WriteLine(darkKnight);
+            Console.WriteLine(bladeKnight);
+            
+        }
+    }
+}
