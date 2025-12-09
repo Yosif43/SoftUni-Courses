2ef02a0d3a5044abc3ecafeaef62f9fcabcc81c6
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-12-09 22:20:22 +0000

    Create Seat.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/16. Abstraction and Polymorphism - Lab/Cars/Seat.cs
@@ -0,0 +1,30 @@
+﻿using Cars;
+using System;
+
+internal class Seat : ICar
+{
+    public Seat(string model, string color)
+    {
+        Model = model;
+        Color = color;
+    }
+
+    public string Model { get; set; }
+    public string Color { get; set; }
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Break!";
+    }
+
+    public override string ToString()
+    {
+        return $"{Color} {GetType().Name} {Model}{Environment.NewLine}" +
+            $"{Start()}{Environment.NewLine}" +
+            $"{Stop()}";
+    }
+}
