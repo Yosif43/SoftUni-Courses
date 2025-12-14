50c4d91604d5e0f457b3bb30b68bfe874f1b4a3c
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-12-14 14:08:06 +0000

    Create AppetizerMenuItem.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/17. Implementing an OOP Hierarchy - Exercise/Ready/AppetizerMenuItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ExerciseOopHierarchy
+{
+	//АРТИКУЛ ОТ МЕНЮТО -> ПРЕДЯСТИЕ
+	public class AppetizerMenuItem : MenuItem
+	{
+		//наследени характеристики: Name, Description, Price
+
+		//конструктор
+		public AppetizerMenuItem(string name, string description, decimal price)
+			: base (name, description, price)
+		{
+			
+		}
+
+        //ToString -> "Appetizer: {base.ToString()}"
+        public override string ToString()
+        {
+            return "Appetizer: " + base.ToString();
+        }
+    }
+}
+
