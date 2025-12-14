d0a3812aed13866edf1abea92e6eaf8d9a3ce154
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-12-14 14:09:25 +0000

    Create Customer.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/17. Implementing an OOP Hierarchy - Exercise/Ready/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExerciseOopHierarchy
+{
+	public class Customer
+	{
+		//характеристики: списък с поръчки, име, имейл
+		private List<Order> _orderHistory = new();
+		public IReadOnlyCollection<Order> OrderHistory => this._orderHistory.AsReadOnly();
+		public string Name { get; set; }
+		public string Email { get; set; }
+
+		//конструктор
+		public Customer (string name, string email)
+		{
+            //нов празен обект
+            //_orderHistor = {}
+            Name = name;
+			Email = email;
+
+		}
+
+		//действия
+		public void AddOrder (Order order)
+		{
+			_orderHistory.Add(order);
+
+        }
+
+	}
+}
+
