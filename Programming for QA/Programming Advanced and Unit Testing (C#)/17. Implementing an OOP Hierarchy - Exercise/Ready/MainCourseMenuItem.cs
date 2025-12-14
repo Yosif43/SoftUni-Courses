86af677abe68d9c9061d18c8f5a7c1b40db67e2b
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-12-14 14:09:52 +0000

    Create MainCourseMenuItem.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/17. Implementing an OOP Hierarchy - Exercise/Ready/MainCourseMenuItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ExerciseOopHierarchy
+{
+	//АРТИКУЛ ОТ МЕНЮТО -> ОСНОВНО ЯСТИЕ
+	public class MainCourseMenuItem : MenuItem
+    {
+        //наследени характеристики: Name, Description, Price
+
+        //конструктор
+        public MainCourseMenuItem(string name, string description, decimal price)
+            : base(name, description, price)
+        {
+
+        }
+
+        //ToString -> "Main Course: {base.ToString()}"
+        public override string ToString()
+        {
+            return "Main Course: " + base.ToString();
+        }
+    }
+}
+
