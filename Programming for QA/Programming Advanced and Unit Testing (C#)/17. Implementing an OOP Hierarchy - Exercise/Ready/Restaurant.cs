1907783e3db55490d0a9680ac710798376f7e929
Yosif43 <yosifyosifov4393@GMAIL.COM>
2026-03-09 17:41:36 +0000

    Create Restaurant.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/17. Implementing an OOP Hierarchy - Exercise/Ready/Restaurant.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExerciseOopHierarchy
+{
+	public class Restaurant
+	{
+		//характеристики: меню (списък с артикули) + клиентела (списък с клиенти)
+		private List<MenuItem> _menu = new();
+		private List<Customer> _customers = new();
+
+		//дейности
+
+		public void AddCustomer(Customer customer)
+		{
+			this._customers.Add(customer);
+		}
+
+        public MenuItem GetMenuItem(int index)
+		{
+			//1. невалиден индекс -> по-малък от първия или по-голям от последния
+			if (index < 0 || index > _menu.Count -1)
+			{
+				//невалиден индекс
+				throw new IndexOutOfRangeException();
+			}
+			//2. валиден индекс
+			return _menu[index];
+		}
+		
+		public void AddMenuItem(MenuItem item)
+		{
+			this._menu.Add(item);
+		}
+
+		public void PlaceOrder(Customer customer, Order order)
+		{
+			customer.AddOrder(order);
+		}
+
+		public void DisplayMenu()
+		{
+			Console.WriteLine("Menu Items:");
+			foreach (MenuItem item in _menu)
+			{
+				Console.WriteLine(item.ToString());
+			}
+		}
+
+		public void DisplayOrderHistory(Customer customer)
+		{
+			//customer = {name: "Ivan", email: "ivan@abv.bg", orderHistory: {..., ..., ...}}
+			Console.WriteLine($"{customer.Name}'s Order History:");
+			foreach(Order order in customer.OrderHistory)
+			{
+				Console.WriteLine($"Order Total: ${order.GetTotal()}");
+				foreach(MenuItem item in order.Items)
+				{
+					Console.WriteLine($"  {item}");
+				}
+			}
+		}
+
+    }
+}
+
