cde53127af4b846778aeea4d69ef32c926fa8eb0
Yosif43 <yosifyosifov4393@GMAIL.COM>
2026-03-09 17:41:24 +0000

    Create Order.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/17. Implementing an OOP Hierarchy - Exercise/Ready/Order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExerciseOopHierarchy
+{
+	public class Order
+	{
+		//ХАРАКТЕРИСТИКИ
+
+		//поле -> описваме / модифицираме артикулите
+		private List<MenuItem> _items = new();
+		//имаме място, където да съхраним нашите артикули
+
+
+		//ИСКАМЕ САМО ДОСТЪП ЗА ЧЕТЕНЕ
+		//поле -> за външния свят
+		public IReadOnlyCollection<MenuItem> Items => this._items.AsReadOnly();
+
+		//ПОВЕДЕНИЕ / ДЕЙНОСТИ
+
+		//1. добавяне на артикул в поръчката
+		public void AddItem (MenuItem menuItem)
+		{
+			this._items.Add(menuItem);
+		}
+
+		//2. изчислим общата сума на поръчката
+		//_items = {12.90, 8.90, 5.60}
+		public decimal GetTotal()
+		{
+			return this._items.Select(item => item.Price).Sum();
+		}
+
+	}
+}
+
