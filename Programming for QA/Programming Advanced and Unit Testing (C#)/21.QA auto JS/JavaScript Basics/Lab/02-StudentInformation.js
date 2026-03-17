d5412c8d5a71c5557bc6bf12904f096bd3c0c831
Yosif43 <yosifyosifov4393@GMAIL.COM>
2026-03-17 19:41:57 +0000

    Create 02-StudentInformation.js

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/21.QA auto JS/JavaScript Basics/Lab/02-StudentInformation.js
@@ -0,0 +1,9 @@
+//Declare function
+function printStudentInformation (name, age, grade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`);
+}
+
+//Invokation function
+printStudentInformation('John', 15, 5.54678);
+printStudentInformation('Steve', 16, 2.1426);
+printStudentInformation('Marry', 12, 6.00);
