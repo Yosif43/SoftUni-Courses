fc9b40d228b234822b296fcd621d3ae9daadcf7d
Yosif43 <yosifyosifov4393@GMAIL.COM>
2026-03-17 19:41:51 +0000

    Create 01-MultiplyNumberBy2.js

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/21.QA auto JS/JavaScript Basics/Lab/01-MultiplyNumberBy2.js
@@ -0,0 +1,9 @@
+//Declare function
+function multiplyNumberByTwo (number) {
+    console.log(number * 2);
+}
+
+//Invokation function
+multiplyNumberByTwo(5);
+multiplyNumberByTwo(2);
+multiplyNumberByTwo(20);
