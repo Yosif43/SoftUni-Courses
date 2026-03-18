883201d36dc1bf42c13f89152bab59547d547bb6
Yosif43 <yosifyosifov4393@GMAIL.COM>
2026-03-18 17:32:32 +0000

    Create 09-Numbers1to5.js

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/21.QA auto JS/JavaScript Basics/Lab/09-Numbers1to5.js
@@ -0,0 +1,17 @@
+//Declare function
+function printNumbers1to5 () {
+
+    //For цикъл
+    //повторение: отпечатваме дадено число
+    //начало: 1
+    //край: 5
+    //промяна: +1
+
+    for (let number = 1; number <= 5; number++) {
+        //действие, което повтаряме
+        console.log(number);
+    }
+}
+
+//Invokation function
+printNumbers1to5();
