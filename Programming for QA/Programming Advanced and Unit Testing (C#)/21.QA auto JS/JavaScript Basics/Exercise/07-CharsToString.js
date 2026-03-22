f6a0f9a3590fa02433c7dbc42e8e969f39f9eb1b
Yosif43 <yosifyosifov4393@GMAIL.COM>
2026-03-22 10:31:14 +0000

    Create 07-CharsToString.js

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/21.QA auto JS/JavaScript Basics/Exercise/07-CharsToString.js
@@ -0,0 +1,13 @@
+//Declare Function
+function constructText (firstSymbol, secondSymbol, thirdSymbol) {
+    //firstSymbol = 'a'
+    //secondSymbol = 'b'
+    //thirdSymbol = 'c'
+
+    const text = firstSymbol + secondSymbol + thirdSymbol;
+    console.log(text);
+}
+
+//Invokation 
+constructText('a', 'b', 'c');
+constructText('%', '2', 'o');
