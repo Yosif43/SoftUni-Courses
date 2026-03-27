056174cfffb16d9fdd710069e039df7ba97249d4
Yosif43 <yosifyosifov4393@GMAIL.COM>
2026-03-27 17:17:14 +0000

    Create 08-math-power.js

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/21.QA auto JS/JavaScript Fundamentals/Lab/08-math-power.js
@@ -0,0 +1,8 @@
+function mathPower(number, power) {
+    let result = Math.pow(number, power);
+    console.log(result);
+}
+
+// Example usage:
+mathPower(2, 8); // Outputs: 256
+mathPower(3, 4); // Outputs: 81
