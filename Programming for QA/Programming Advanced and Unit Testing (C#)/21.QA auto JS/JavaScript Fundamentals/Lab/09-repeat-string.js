c68b76b3eaeb7cfd62a3d48bdf59350f68b30329
Yosif43 <yosifyosifov4393@GMAIL.COM>
2026-03-27 17:18:29 +0000

    Create 09-repeat-string.js

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/21.QA auto JS/JavaScript Fundamentals/Lab/09-repeat-string.js
@@ -0,0 +1,7 @@
+function repeatString(str, n) {
+    return str.repeat(n);
+}
+
+// Example usage:
+console.log(repeatString("abc", 3)); // Outputs: abcabcabc
+console.log(repeatString("String", 2)); // Outputs: StringString
