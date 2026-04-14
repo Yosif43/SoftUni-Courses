710cfce29017741d64831d5cbb92259f69fc84d2
Yosif43 <117854744+Yosif43@users.noreply.github.com>
2026-04-14 17:22:54 +0100

    .

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/21.QA auto JS/Unit Testing with JavaScript/Exercise/ESModules/index.mjs
@@ -0,0 +1,13 @@
+import {
+    multiply,
+    subtract,
+    sum,
+    divide,
+} from './calculator.mjs';
+import * as calculator from './calculator.mjs';
+
+console.log(sum(2, 5));
+console.log(subtract(2, 5));
+console.log(multiply(2, 5));
+console.log(divide(2, 5));
+console.log(calculator.sum(5, 10));
