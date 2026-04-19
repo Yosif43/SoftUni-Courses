d846fd2c3785f3af971ca18e3ab2cdab9e9f155f
Yosif43 <117854744+Yosif43@users.noreply.github.com>
2026-04-19 16:24:33 +0100

    Create StringModifier.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/Programming Advanced for QA Regular Exam 13 April 2024/01. String Modifier/TestApp/StringModifier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace TestApp;
+public class StringModifier
+{
+    public static string Modify(string input)
+    {
+        string[] words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        StringBuilder sb = new();
+
+        foreach (string word in words)
+        {
+            if (word.Length % 2 == 0)
+            {
+                sb.Append($"{word.ToUpper()} ");
+            }
+            else
+            {
+                sb.Append($"{word.ToLower()} ");
+            }
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
+
