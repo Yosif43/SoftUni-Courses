bca72270a6c8c1b1769be66df75223771950d9d7
Yosif43 <117854744+Yosif43@users.noreply.github.com>
2026-04-19 16:25:12 +0100

    Create SentenceAnalyzer.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/Programming Advanced for QA Regular Exam 13 April 2024/02. Sentence Analyzer/TestApp/SentenceAnalyzer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp;
+
+public class SentenceAnalyzer
+{
+    public static Dictionary<string, int> Analyze(string sentence)
+    {
+        Dictionary<string, int> symbols = new Dictionary<string, int>();
+
+        foreach (char symbol in sentence)
+        {
+            if (char.IsWhiteSpace(symbol))
+            {
+                continue;
+            }
+            if (char.IsLetter(symbol))
+            {
+                if (symbols.ContainsKey("letters") == false)
+                {
+                    symbols.Add("letters", 0);
+                }
+
+                symbols["letters"]++;
+            }
+            else if (char.IsDigit(symbol))
+            {
+                if (symbols.ContainsKey("digits") == false)
+                {
+                    symbols.Add("digits", 0);
+                }
+
+                symbols["digits"]++;
+            }
+            else
+            {
+                if (symbols.ContainsKey("special characters") == false)
+                {
+                    symbols.Add("special characters", 0);
+                }
+
+                symbols["special characters"]++;
+            }
+        }
+
+        return symbols;
+    }
+}
+
