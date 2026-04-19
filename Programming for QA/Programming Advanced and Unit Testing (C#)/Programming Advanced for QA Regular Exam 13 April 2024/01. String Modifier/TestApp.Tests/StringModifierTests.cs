eb5f9222e206be06b39bc9d148f67e6b57720766
Yosif43 <117854744+Yosif43@users.noreply.github.com>
2026-04-19 16:24:45 +0100

    Create StringModifierTests.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/Programming Advanced for QA Regular Exam 13 April 2024/01. String Modifier/TestApp.Tests/StringModifierTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+[TestFixture]
+public class StringModifierTests
+{
+    [Test]
+    public void Test_Modify_EmptyString_ReturnsEmptyString()
+    {
+        // Arrange
+        string input = "";
+        string expected = "";
+
+        // Act
+        string result = StringModifier.Modify(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Modify_SingleWordWithEvenLength_ReturnsUppperCaseWord()
+    {
+        // Arrange
+        string input = "even";
+        string expected = "EVEN";
+
+        // Act
+        string result = StringModifier.Modify(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+
+    }
+
+    [Test]
+    public void Test_Modify_SingleWordWithOddLength_ReturnsToLowerCaseWord()
+    {
+        // Arrange
+        string input = "oDd";
+        string expected = "odd";
+
+        // Act
+        string result = StringModifier.Modify(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+
+    }
+
+    [Test]
+    public void Test_Modify_MultipleWords_ReturnsModifiedString()
+    {
+        // Arrange
+        string input = "This is a Test";
+        string expected = "THIS IS a TEST";
+
+        // Act
+        string result = StringModifier.Modify(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+
+    }
+}
+
