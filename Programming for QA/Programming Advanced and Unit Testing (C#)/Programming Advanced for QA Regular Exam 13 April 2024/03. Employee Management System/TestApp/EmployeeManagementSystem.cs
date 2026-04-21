89cc3fddd82aa8a5660bdf47bcaa6315f8d7fa8e
Yosif43 <117854744+Yosif43@users.noreply.github.com>
2026-04-21 17:32:50 +0100

    Create EmployeeManagementSystem.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/Programming Advanced for QA Regular Exam 13 April 2024/03. Employee Management System/TestApp/EmployeeManagementSystem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestApp;
+
+public class EmployeeManagementSystem
+{
+    private List<string> employees;
+
+    public EmployeeManagementSystem()
+    {
+        employees = new List<string>();
+    }
+
+    public int EmployeeCount { get => employees.Count; }
+
+    public void AddEmployee(string employeeName)
+    {
+        if (string.IsNullOrWhiteSpace(employeeName))
+        {
+            throw new ArgumentException("Employee name cannot be empty or whitespace.");
+        }
+
+        employees.Add(employeeName);
+    }
+
+    public void RemoveEmployee(string employeeName)
+    {
+        if (!employees.Contains(employeeName))
+        {
+            throw new ArgumentException("Employee not found in the system.");
+        }
+
+        employees.Remove(employeeName);
+    }
+
+    public List<string> GetAllEmployees()
+    {
+        return employees;
+    }
+}
+
