c655cd39746f857e21847956df1895ca4b61b5b8
Yosif43 <117854744+Yosif43@users.noreply.github.com>
2026-04-24 17:49:30 +0100

    Create AcronymGeneratorTests.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/Programming Advanced for QA Retake Exam 22 December 2023/01. Acronym Generator/TestApp.Tests/AcronymGeneratorTests.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+[TestFixture]
+public class AcronymGeneratorTests
+{
+    [Test]
+    public void Test_GenerateAcronym_EmptyString_ReturnsEmptyString()
+    {
+        // TODO: finish the test
+    }
+
+    [Test]
+    public void Test_GenerateAcronym_SingleWord_ReturnsUpperCaseFirstLetter()
+    {
+        // TODO: finish the test
+    }
+
+    [Test]
+    public void Test_GenerateAcronym_MultipleWords_ReturnsUpperCaseFirstLetters()
+    {
+        // TODO: finish the test
+    }
+
+    [Test]
+    public void Test_GenerateAcronym_WordsWithNonLetters_ReturnsAcronymWithoutNonLetters()
+    {
+        // TODO: finish the test
+    }
+
+    [Test]
+    public void Test_GenerateAcronym_PhraseWithSpecialCharacters_ReturnsUpperCaseFirstLetters()
+    {
+        // TODO: finish the test
+    }
+}
