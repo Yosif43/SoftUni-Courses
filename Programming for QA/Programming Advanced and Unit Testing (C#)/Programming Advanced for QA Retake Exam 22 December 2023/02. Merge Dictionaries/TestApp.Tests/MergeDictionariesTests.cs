cdb6fb87d3b7c8a754204e4b79c27f05e5cf9c98
Yosif43 <117854744+Yosif43@users.noreply.github.com>
2026-04-25 15:15:56 +0100

    Create MergeDictionariesTests.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/Programming Advanced for QA Retake Exam 22 December 2023/02. Merge Dictionaries/TestApp.Tests/MergeDictionariesTests.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+[TestFixture]
+public class MergeDictionariesTests
+{
+    [Test]
+    public void Test_Merge_TwoEmptyDictionaries_ReturnsEmptyDictionary()
+    {
+        // TODO: finish this test
+    }
+
+    [Test]
+    public void Test_Merge_OneEmptyDictionaryAndOneNonEmptyDictionary_ReturnsNonEmptyDictionary()
+    {
+        // TODO: finish this test
+    }
+
+    [Test]
+    public void Test_Merge_TwoNonEmptyDictionaries_ReturnsMergedDictionary()
+    {
+        // TODO: finish this test
+    }
+
+    [Test]
+    public void Test_Merge_OverlappingKeys_ReturnsMergedDictionaryWithValuesFromDict2()
+    {
+        // TODO: finish this test
+    }
+}
