8db767a08fab3ef15a3fa12a2cdfdaa24804384f
Yosif43 <117854744+Yosif43@users.noreply.github.com>
2026-04-25 15:16:49 +0100

    Create Book.cs

---
+++ Programming for QA/Programming Advanced and Unit Testing (C#)/Programming Advanced for QA Retake Exam 22 December 2023/03. Library/TestApp/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace TestApp.Library
+{
+    public class Book
+    {
+        public Book(string isbn, string title, string author)
+        {
+            this.Isbn = isbn;
+            this.Title = title;
+            this.Author = author;
+        }
+
+        public string Isbn { get; }
+
+        public string Title { get; }
+
+        public string Author { get; }
+    }
+}
